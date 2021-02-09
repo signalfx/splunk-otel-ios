3adb3545d7f8d9007cfc00842aad4061b19f6535
John Bley <jbley@splunk.com>
2021-02-09 11:21:06 -0500

    Progress on a cli build...

---
+++ fullbuild.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# FIXME obviously not a full build yet
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Debug build
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Debug test
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Release build
+
+echo "========= Congratulations! ========="
