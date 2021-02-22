6a6f8bd86f2a9b45092994a5ae335c6b63dfc31b
John Bley <jbley@splunk.com>
2021-02-22 13:14:41 -0500

    Clean up some comments around viewController capture

--- SplunkRumWorkspace/SplunkRum/SplunkRum/InstrumentationUtils.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/InstrumentationUtils.swift
@@ -13,6 +13,7 @@ import OpenTelemetrySdk
 private func addUIFields(span: ReadableSpan) {
     let wins = UIApplication.shared.windows
     if !wins.isEmpty {
+        // windows are arranged in z-order, with topmost (e.g. popover) being the last in array
         let vc = wins[wins.count-1].rootViewController
         if vc != nil {
             let clazz = object_getClass(vc)
@@ -21,10 +22,11 @@ private func addUIFields(span: ReadableSpan) {
                 // FIXME demangle swift names
                 span.setAttribute(key: "screen.name", value: className)
             }
-            // FIXME fallback to description?
-            print(vc!.description)
+            // FIXME SwiftUI UIHostingController vc when cast has a "rootView" var which does
+            // not appear to be accessible generically
         }
     }
+    // FIXME others?
 }
 
 func addPreSpanFields(span: ReadableSpan) {
