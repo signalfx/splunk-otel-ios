6f0831601f0df3dec6ff7f0f1a3362cd731a3682
John Bley <jbley@splunk.com>
2021-02-24 11:10:45 -0500

    Run swiftlint in fullbuild.sh

--- fullbuild.sh
+++ fullbuild.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 set -e
 # FIXME obviously not a full build yet
+swiftlint --strict
 xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Debug build
 xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Debug test
 xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Release build
