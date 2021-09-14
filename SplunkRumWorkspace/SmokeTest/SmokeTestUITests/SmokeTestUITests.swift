f308a4e2c1caa23dbe227d220248d9784b45358c
John Bley <jbley@splunk.com>
2021-09-13 21:13:19 -0400

    More smoketest testing (#26)
    * Remove smoke unit tests as we only need the ui ones
    
    * Check some basic startup spans

--- SplunkRumWorkspace/SmokeTest/SmokeTestUITests/SmokeTestUITests.swift
+++ SplunkRumWorkspace/SmokeTest/SmokeTestUITests/SmokeTestUITests.swift
@@ -18,9 +18,15 @@ limitations under the License.
 import XCTest
 import Swifter
 
+// "annotations":[{"value":"process.start","timestamp":1631578180579894}
 struct TestZipkinSpan: Decodable {
     var name: String
     var tags: [String: String]
+    var annotations: [TestZipkinAnnotation]
+}
+struct TestZipkinAnnotation: Decodable {
+    var value: String
+    var timestamp: Int64
 }
 var receivedSpans: [TestZipkinSpan] = []
 
@@ -54,8 +60,34 @@ class SmokeTestUITests: XCTestCase {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
         print(receivedSpans)
-        XCTAssert(receivedSpans.count > 1)
+        XCTAssert(receivedSpans.count > 2)
+        let srInit = receivedSpans.first(where: { (span) -> Bool in
+            return span.name == "SplunkRum.initialize"
+        })
+        XCTAssertNotNil(srInit)
+        let appStart = receivedSpans.first(where: { (span) -> Bool in
+            return span.name == "AppStart"
+        })
+        XCTAssertNotNil(appStart)
+        XCTAssertNotNil(appStart?.annotations.first(where: { (annot) -> Bool in
+            return annot.value == "process.start"
+        }))
+        XCTAssertNotNil(appStart?.annotations.first(where: { (annot) -> Bool in
+            return annot.value == "UIApplicationDidFinishLaunchingNotification"
+        }))
+        XCTAssertNotNil(appStart?.annotations.first(where: { (annot) -> Bool in
+            return annot.value == "UIApplicationWillEnterForegroundNotification"
+        }))
+        XCTAssertNotNil(appStart?.annotations.first(where: { (annot) -> Bool in
+            return annot.value == "UIApplicationDidBecomeActiveNotification"
+        }))
+        let presTrans = receivedSpans.first(where: { (span) -> Bool in
+            return span.name == "PresentationTransition"
+        })
+        XCTAssertNotNil(presTrans)
+        XCTAssertEqual("ViewController", presTrans?.tags["screen.name"]?.description)
 
+        // FIXME interaction and screen name change
     }
 
 }
