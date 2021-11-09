d4d12564cc19b46043b0bfad4da4bd2f41b61624
John Bley <jbley@splunk.com>
2021-11-09 10:39:21 -0500

    Bump version string to 0.5.0

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -21,7 +21,7 @@ import ZipkinExporter
 import StdoutExporter
 import WebKit
 
-let SplunkRumVersionString = "0.4.0"
+let SplunkRumVersionString = "0.5.0"
 
 /**
  Optional configuration for SplunkRum.initialize()
