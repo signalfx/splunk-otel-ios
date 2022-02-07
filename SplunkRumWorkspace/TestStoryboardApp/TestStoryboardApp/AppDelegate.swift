4f187b9dabbba2983e87c1a03c3dc7b59139a4fa
Shraddha Hattimare <shattimare@splunk.com>
2022-02-07 14:31:50 +0530

    feature/swat-4136
    integrate googlemapSDK

--- SplunkRumWorkspace/TestStoryboardApp/TestStoryboardApp/AppDelegate.swift
+++ SplunkRumWorkspace/TestStoryboardApp/TestStoryboardApp/AppDelegate.swift
@@ -19,6 +19,7 @@ import UIKit
 // Why not "import SplunkOtel"?  Because this links as a local framework, not as a swift package.
 // FIXME align the framework name and directory names with the swift package name at some point
 import SplunkRum
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -26,21 +27,26 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         SplunkRum.initialize(beaconUrl: "http://127.0.0.1:9080/api/v2/spans", rumAuth: "FAKE_RUM_AUTH", options: SplunkRumOptions(allowInsecureBeacon: true, debug: true))
+        
+        GMSServices.provideAPIKey("AIzaSyCBlTdA1j2D8x88PMVWG4NNM_PsBfeUj00")
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+ 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
 
+    @available(iOS 13.0, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
+   
 
 }
