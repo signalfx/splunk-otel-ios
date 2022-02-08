b25f9eebd57ce79d35d1fee412f54d7281f684c3
Shraddha Hattimare <shattimare@splunk.com>
2022-02-08 10:42:46 +0530

    swat-4136
    Reproduced crash

--- SplunkRumWorkspace/TestStoryboardApp/TestStoryboardApp/AppDelegate.swift
+++ SplunkRumWorkspace/TestStoryboardApp/TestStoryboardApp/AppDelegate.swift
@@ -23,6 +23,7 @@ import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
