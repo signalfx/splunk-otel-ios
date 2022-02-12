7d5a9d58d32e368a0dbb95bcc30dd99ae48c6cbc
Piyush23786 <piyushp@splunk.com>
2022-02-12 10:25:39 +0530

    screen render time
    Capture screen render time events.

---
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/ScreenFrames.swift
@@ -0,0 +1,62 @@
+//
+/*
+Copyright 2021 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+	
+
+import Foundation
+import QuartzCore
+
+class ScreenFrames: NSObject {
+
+    private var latestTimeUpdated: CFTimeInterval = CACurrentMediaTime()
+    private var startedTime: CFTimeInterval = CACurrentMediaTime()
+
+    private var frameCount: Int = 0
+    private var currentIteration: Int = 0
+
+    private var displayLink: CADisplayLink!
+
+    private func setUpDisplayLink() {
+        displayLink = CADisplayLink(target: self, selector: #selector(update))
+        displayLink.preferredFramesPerSecond = 60
+        startedTime = CACurrentMediaTime()
+        displayLink.add(to: .main, forMode: RunLoop.Mode.common)
+    }
+
+    @objc private func update() {
+       // print("refresh : \(CACurrentMediaTime())")
+        let actualFramesPerSecond = 1 / (displayLink.targetTimestamp - displayLink.timestamp)
+        print("actualFramesPerSecond : \(String(format: "%.03f", actualFramesPerSecond))")
+
+        let currentTime: CFTimeInterval = CACurrentMediaTime()
+        let timeElapsed: Double = currentTime - startedTime
+
+        let iteration = Int(timeElapsed)
+        if currentIteration == iteration {
+            frameCount += 1
+        } else {
+
+            frameCount = 0
+            currentIteration = iteration
+        }
+
+        let timeDiffBetweenUpdates: CFTimeInterval = currentTime - latestTimeUpdated
+        print("timeDiffBetweenUpdates : \(String(format: "%.03f", timeDiffBetweenUpdates))")
+
+        latestTimeUpdated = currentTime
+
+    }
+}
