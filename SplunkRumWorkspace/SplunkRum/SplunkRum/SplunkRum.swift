58f28328208d9a538e4f6c560f70f2f109e88af3
John Bley <jbley@splunk.com>
2022-02-15 09:06:37 -0500

    v0.5.2 (#55)

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -21,7 +21,7 @@ import ZipkinExporter
 import StdoutExporter
 import WebKit
 
-let SplunkRumVersionString = "0.5.1"
+let SplunkRumVersionString = "0.5.2"
 
 /**
  Optional configuration for SplunkRum.initialize()
