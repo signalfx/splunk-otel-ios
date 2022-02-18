85cdedc3cc54d918f41a31ca0831f3705db83aa1
Piyush23786 <piyushp@splunk.com>
2022-02-18 11:05:03 +0530

    swiftlint warning
    remove swiftlint warning.

--- SplunkRumWorkspace/SmokeTest/SmokeTest/ViewController.swift
+++ SplunkRumWorkspace/SmokeTest/SmokeTest/ViewController.swift
@@ -19,30 +19,20 @@ import UIKit
 import WebKit
 import SplunkRum
 
-class ViewController: UIViewController, WKUIDelegate,UITableViewDelegate,UITableViewDataSource {
-
-   
-        @IBOutlet var tableView: UITableView!
-        let cellReuseIdentifier = "cell"
-    
+class ViewController: UIViewController, WKUIDelegate, UITableViewDelegate, UITableViewDataSource {
+    @IBOutlet var tableView: UITableView!
+    let cellReuseIdentifier = "cell"
     override func viewDidLoad() {
         super.viewDidLoad()
         self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
         tableView.delegate = self
         tableView.dataSource = self
-        
     }
-    
-    // number of rows in table view
         func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
             return 100
         }
-        
-        // create a cell for each table view row
         func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            
-            // create a new cell if needed or reuse an old one
-            let cell:UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+            let cell: UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
             cell.textLabel?.text = "Row \((indexPath as NSIndexPath).row)"
             cell.textLabel?.layer.shadowColor = UIColor.black.cgColor
             cell.textLabel?.layer.shadowRadius = 3.0
@@ -51,19 +41,13 @@ class ViewController: UIViewController, WKUIDelegate,UITableViewDelegate,UITable
             cell.textLabel?.layer.masksToBounds = false
             return cell
         }
-        
-        // method to run when table view cell is tapped
-        func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-            
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         }
-    
-    
     override func viewDidAppear(_ animated: Bool) {
        sleep(2)
     }
 
-    @IBAction
-    func clickMe() {
+    @IBAction func clickMe() {
         print("I was clicked!")
         SplunkRum.setScreenName("CustomScreenName")
 
