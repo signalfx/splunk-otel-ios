76cb500f0084956001b21e159f3b6d7de520d68b
Shraddha Hattimare <shattimare@splunk.com>
2022-03-23 15:36:40 +0530

    feature/APMI-2313- Disk caching
    changed test case.

--- SplunkRumWorkspace/SplunkRum/SplunkRumTests/UtilsTests.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRumTests/UtilsTests.swift
@@ -196,12 +196,12 @@ class UtilsTests: XCTestCase {
         _ = re.export(spans: fiftySpans)
         _ = re.export(spans: fiftySpans)
         _ = re.export(spans: fiftySpans)
-        XCTAssertEqual(0, localSpans.count)
+        XCTAssertEqual(150, localSpans.count)
 
         // now reconnected; next export of 50 should send total of 150 (last 100 retried)
         te.exportSucceeds = true
         _ = re.export(spans: fiftySpans)
-        XCTAssertEqual(150, localSpans.count)
+        XCTAssertEqual(250, localSpans.count)
         localSpans.removeAll()
 
     }
