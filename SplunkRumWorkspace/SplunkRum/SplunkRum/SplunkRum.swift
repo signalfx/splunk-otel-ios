e369c1c11aa67dd809171e3f27ea0b90b9919683
Shraddha Hattimare <shattimare@splunk.com>
2022-04-05 12:11:36 +0530

    Update SplunkRum.swift

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -147,7 +147,6 @@ var splunkRumInitializeCalledTime = Date()
                 - Parameter options: Non-required configuration toggles for various features.  See SplunkRumOptions struct for details.
      
      */
-    
     @objc public class func initialize(beaconUrl: String, rumAuth: String, options: SplunkRumOptions? = nil) {
         if !Thread.isMainThread {
             print("SplunkRum: Please call SplunkRum.initialize only on the main thread")
