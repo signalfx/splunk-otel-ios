18022c84dc851f8686794736604868f78a5bfef5
Shraddha Hattimare <shattimare@splunk.com>
2022-04-06 11:44:42 +0530

    change in retry exporter

--- SplunkRumWorkspace/SplunkRum/SplunkRum/RetryExporter.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/RetryExporter.swift
@@ -29,64 +29,13 @@ class RetryExporter: SpanExporter {
         self.proxy = proxy
     }
 
-  /*  func attemptPendingExport() -> SpanExporterResultCode {
-        if pending.isEmpty {
-            return .success
-        }
-        let result = proxy.export(spans: pending)
-        if result == .success {
-            pending.removeAll(keepingCapacity: true)
-        }
-        return result
-    }
-
-    func addToPending(_ spans: [SpanData]) {
-        pending.append(contentsOf: spans)
-        if pending.count > MAX_PENDING_SPANS {
-            pending.removeFirst(pending.count - MAX_PENDING_SPANS)
-        }
-    }*/
-
-  /*  func export(spans: [SpanData]) -> SpanExporterResultCode {
-        var result = attemptPendingExport()
-        if result == .failure {
-            addToPending(spans)
-            return .failure
-        }
-        result = proxy.export(spans: spans)
-        if result == .failure {
-            addToPending(spans)
-            return .failure
-        }
-        return .success
-    }
-
-    func flush() -> SpanExporterResultCode {
-        _ = attemptPendingExport()
-        return proxy.flush()
-    }
-
-    func shutdown() {
-        proxy.shutdown()
-    }*/
-
-    func export(spans: [SpanData]) -> SpanExporterResultCode {
+  func export(spans: [SpanData]) -> SpanExporterResultCode {
         var result = attemptDBExport()
-      /*  if result == .failure {
-            CoreDataManager.shared.insertSpanValue(spans) // seperate values
-            return .failure
-        }*/
         result = proxy.export(spans: spans)  // call zipkinexporter
         if result == .failure {
            CoreDataManager.shared.insertSpanValue(spans) // seperate values
-
-//            for _ in 0...100000 {
-//                CoreDataManager.shared.insertSpanIntoDB(spans)
-//            }
-//            CoreDataManager.shared.getStoreInformation()
             return .failure
         }
-
         return .success
     }
 
@@ -116,11 +65,5 @@ class RetryExporter: SpanExporter {
             CoreDataManager.shared.deleteSpanData(spans: dbspans)
             return .success
         }
-      /*  let result = proxy.export(spans: dbspans)  // call zipkinspan
-        if result == .success {
-            //delete exported span only
-            CoreDataManager.shared.deleteSpanData(spans: dbspans)
-        }
-        return result*/
     }
 }
