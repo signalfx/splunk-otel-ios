70e671dc02d309a6d52f972d4b843f168eae2be6
Shraddha Hattimare <shattimare@splunk.com>
2022-04-06 18:09:56 +0530

    feature/APMI-2313
    remove comment from .sh file

--- fullbuild.sh
+++ fullbuild.sh
@@ -6,10 +6,10 @@ xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme 
 xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SplunkRum -configuration Release build
 
 # Now try to do a swift build to ensure that the package dependencies are properly in synch
-#rm -rf ./.build
-#swift build -v -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios11.0-simulator"
-#rm -rf ./.build
+rm -rf ./.build
+swift build -v -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphonesimulator --show-sdk-path`" -Xswiftc "-target" -Xswiftc "x86_64-apple-ios11.0-simulator"
+rm -rf ./.build
 # repeat targeting a real device
-#swift build -v -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphoneos --show-sdk-path`" -Xswiftc "-target" -Xswiftc "arm64-apple-ios14.0"
+swift build -v -Xswiftc "-sdk" -Xswiftc "`xcrun --sdk iphoneos --show-sdk-path`" -Xswiftc "-target" -Xswiftc "arm64-apple-ios14.0"
 
 echo "========= Congratulations! ========="
