7b9a87e4ebe625ecb099befc0844c77eda18083c
Piyush23786 <piyushp@splunk.com>
2022-04-13 15:16:31 +0530

    Swat-4463
    checking with new Xcode 13.3.1

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.6
+// swift-tools-version:5.3
 
 import PackageDescription
 
@@ -6,30 +6,30 @@ let package = Package(
     name: "SplunkOtel",
     platforms: [
         .iOS(.v11),
-	.macOS(.v10_13)
+    .macOS(.v10_13)
     ],
     products: [
         .library(name: "SplunkOtel", targets: ["SplunkOtel"])
     ],
     dependencies: [
-        .package(url:"https://github.com/open-telemetry/opentelemetry-swift", from: "1.1.0"),
-	.package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
+        .package(name: "opentelemetry-swift", url:"https://github.com/open-telemetry/opentelemetry-swift", from: "1.1.0"),
+    .package(url: "https://github.com/devicekit/DeviceKit.git", from: "4.0.0"),
     ],
     targets: [
         .target(
             name: "SplunkOtel",
             dependencies: [
-		.product(name: "OpenTelemetryApi", package:"opentelemetry-swift"),
-		.product(name: "OpenTelemetrySdk", package:"opentelemetry-swift"),
-		.product(name: "StdoutExporter", package:"opentelemetry-swift"),
-		.product(name: "ZipkinExporter", package:"opentelemetry-swift"),
-		.product(name: "DeviceKit", package: "DeviceKit")
+        .product(name: "OpenTelemetryApi", package:"opentelemetry-swift"),
+        .product(name: "OpenTelemetrySdk", package:"opentelemetry-swift"),
+        .product(name: "StdoutExporter", package:"opentelemetry-swift"),
+        .product(name: "ZipkinExporter", package:"opentelemetry-swift"),
+        .product(name: "DeviceKit", package: "DeviceKit")
             ],
             path: "SplunkRumWorkspace/SplunkRum",
             exclude: [
-		"SplunkRumTests",
-		"SplunkRum/SplunkRum.h",
-		"SplunkRum/Info.plist"
+        "SplunkRumTests",
+        "SplunkRum/SplunkRum.h",
+        "SplunkRum/Info.plist"
             ],
             sources: [
                 "SplunkRum",
