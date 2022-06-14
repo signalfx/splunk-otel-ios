3de7cdf2b73f2591642b4975278f9150c36412b6
John Bley <jbley@splunk.com>
2022-06-14 09:10:35 -0500

    0.7.0 (#84)

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -21,7 +21,7 @@ import ZipkinExporter
 import StdoutExporter
 import WebKit
 
-let SplunkRumVersionString = "0.6.0"
+let SplunkRumVersionString = "0.7.0"
 
 /**
  Optional configuration for SplunkRum.initialize()
