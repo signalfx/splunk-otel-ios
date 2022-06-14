7455ce59f7d3015e8546135c557012e6d31ac6f2
shraddha hattimare <98871176+shattimare@users.noreply.github.com>
2022-06-14 15:22:29 +0530

    test
    test

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -206,6 +206,7 @@ var splunkRumInitializeCalledTime = Date()
         // not initializeAppLifecycleInstrumentation, done at end of AppStart
         srInit.end()
         initialized = true
+        
         print("SplunkRum.initialize() complete")
         return true
 
