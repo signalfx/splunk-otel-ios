dfa5bdc9c6ee7e3b39177ab76706e9693b560621
shattimare <98871176+shattimare@users.noreply.github.com>
2022-06-21 12:29:26 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -7,3 +7,6 @@ set -x
 TEST_DEVICE=`xcrun xctrace list devices 2>&1  | grep iPhone | head -1  | sed 's/.*[([]//' | sed 's/.$//'`
 
 xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test -destination platform="iOS Simulator,id=$TEST_DEVICE"
+
+zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find /Users/runner/Library/Developer/Xcode/DerivedData/SplunkRumWorkspace-dzmkiduinguystddyqrorblmaahj/Build/Products/Debug-iphonesimulator/SmokeTest.app -type f)
+pwd
