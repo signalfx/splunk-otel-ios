f1a76d4e82dfe6791a66fe30dd732cee9e164929
shattimare <98871176+shattimare@users.noreply.github.com>
2022-06-22 10:26:19 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -6,8 +6,9 @@ set -x
 # (different samples of the output of this have [] or () as delimiters of the last (id) field)
 TEST_DEVICE=`xcrun xctrace list devices 2>&1  | grep iPhone | head -1  | sed 's/.*[([]//' | sed 's/.$//'`
 
+rm -rf ./build
 xcodebuild -workspace SmokeTest/SmokeTest.xcworkspace \-scheme SmokeTest clean archive -configuration release \-sdk iphoneos -archivePath SmokeTest.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO
-xcodebuild -exportArchive -archivePath  SmokeTest.xcarchive \-exportOptionsPlist  SmokeTest/exportOptions.plist \-exportPath  SmokeTest.ipa
+# xcodebuild -exportArchive -archivePath  SmokeTest.xcarchive \-exportOptionsPlist  SmokeTest/exportOptions.plist \-exportPath  SmokeTest.ipa
 
 zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find /Users/runner/Library/Developer/Xcode/DerivedData/SplunkRumWorkspace-dzmkiduinguystddyqrorblmaahj/Build/Products/Debug-iphonesimulator/SmokeTest.app -type f)
 pwd
