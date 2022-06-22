a7823d53df62b303764af7e1bbca6f56a0d66ae9
shattimare <98871176+shattimare@users.noreply.github.com>
2022-06-22 17:19:33 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -10,7 +10,7 @@ BUILD_PATH="Build/Products/Debug-iphonesimulator"
 BUILD_NAME="SmokeTest.app"
 
 rm -rf ./build
-xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test -destination platform="iOS Simulator,id=$TEST_DEVICE" -derivedDataPath SmokeBuild
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug -destination platform="iOS Simulator,id=$TEST_DEVICE" -derivedDataPath SmokeBuild
 
 zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find ~/$BUILD_FOLDER/$BUILD_PATH/$BUILD_NAME -type f)
 pwd
