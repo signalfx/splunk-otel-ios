09ac3a8dc28cc584549810b2836f6672211da3ea
shattimare <98871176+shattimare@users.noreply.github.com>
2022-06-22 16:56:00 +0530

    Update smoketest.sh

--- smoketest.sh
+++ smoketest.sh
@@ -5,10 +5,12 @@ set -x
 # This gobbledygook gets a list of simulator iPhones and picks the id of the first one
 # (different samples of the output of this have [] or () as delimiters of the last (id) field)
 TEST_DEVICE=`xcrun xctrace list devices 2>&1  | grep iPhone | head -1  | sed 's/.*[([]//' | sed 's/.$//'`
+BUILD_FOLDER: work/DemoTest/DemoTest/SmokeBuild
+BUILD_PATH: Build/Products/Debug-iphonesimulator
+BUILD_NAME: PasswordEncryption.app
 
 rm -rf ./build
-xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace \-scheme SmokeTest clean archive -configuration release \-sdk iphoneos -archivePath SmokeTest.xcarchive CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO ONLY_ACTIVE_ARCH=NO
-# xcodebuild -exportArchive -archivePath  SmokeTest.xcarchive \-exportOptionsPlist  SmokeTest/exportOptions.plist \-exportPath  SmokeTest.ipa
+xcodebuild -workspace SplunkRumWorkspace/SplunkRumWorkspace.xcworkspace -scheme SmokeTest -configuration Debug test -destination platform="iOS Simulator,id=$TEST_DEVICE" -derivedDataPath SmokeBuild
 
-zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find /Users/runner/Library/Developer/Xcode/DerivedData/SplunkRumWorkspace-dzmkiduinguystddyqrorblmaahj/Build/Products/Debug-iphonesimulator/SmokeTest.app -type f)
+zip ${GITHUB_WORKSPACE}/SmokeTest.zip $(find ~/$BUILD_FOLDER/$BUILD_PATH/$BUILD_NAME -type f)
 pwd
