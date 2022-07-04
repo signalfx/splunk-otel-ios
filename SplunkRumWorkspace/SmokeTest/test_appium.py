2cf93410319e9244a09651f83ae89a4d59851b23
shattimare <98871176+shattimare@users.noreply.github.com>
2022-07-04 17:37:18 +0530

    Update test_appium.py

--- SplunkRumWorkspace/SmokeTest/test_appium.py
+++ SplunkRumWorkspace/SmokeTest/test_appium.py
@@ -26,6 +26,7 @@ class HybridIOSTests(unittest.TestCase):
         caps['sauce:options']['build'] = 'Platform Configurator Build ' + currentDate
         caps['sauce:options']['name'] = 'Platform Configurator Job ' + currentTime
         caps['sauce:options']['proxy'] = {"proxyType": "manual","httpProxy": "http://localhost:8080"}
+        caps['sauce:options']['proxy-localhost'] = True
         
         url = 'https://oauth-shraddha.hattimare-46ddf:8d45c406-3a39-4da8-9fb9-b890a255848c@ondemand.us-west-1.saucelabs.com:443/wd/hub'
         self.driver=webdriver.Remote(url,caps)
