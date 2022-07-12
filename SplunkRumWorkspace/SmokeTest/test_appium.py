0c485b716febfb8e9b7a4287da610fcb04a74a04
shattimare <98871176+shattimare@users.noreply.github.com>
2022-07-12 16:14:46 +0530

    Update test_appium.py

--- SplunkRumWorkspace/SmokeTest/test_appium.py
+++ SplunkRumWorkspace/SmokeTest/test_appium.py
@@ -54,159 +54,159 @@ class IOSTests(unittest.TestCase):
             message='Span validation failed',
         )
 
-#     ''' 
-#     Generating the POST network request with the URLSession and Validating the network span data.
-#     '''
-#     def test_API_PostClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"URLSession").click();
-#         self.driver.find_element(By.ID,"post").click();
-#         self.validate_span();
-
-#     ''' 
-#     Generating the GET network request with the URLSession and Validating the network span data.
-#     '''
-#     def test_API_GetClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"URLSession").click();
-#         self.driver.find_element(By.ID,"get").click();
-#         self.validate_span();
-
-#     ''' 
-#     Generating the PUT network request with the URLSession and Validating the network span data.
-#     '''
-#     def test_API_PutClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"URLSession").click();
-#         self.driver.find_element(By.ID,"put").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the DELETE network request with the URLSession and Validating the network span data.
-#     '''
-#     def test_API_DeleteClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"URLSession").click();
-#         self.driver.find_element(By.ID,"delete").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the POST network request with the Alamofire and Validating the network span data.
-#     '''
-#     def test_Alamofire_PostClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"Alamofire").click();
-#         self.driver.find_element(By.ID,"post").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the GET network request with the Alamofire and Validating the network span data.
-#     '''    
-#     def test_Alamofire_GetClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"Alamofire").click();
-#         self.driver.find_element(By.ID,"get").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the PUT network request with the Alamofire and Validating the network span data.
-#     '''
-#     def test_Alamofire_PutClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"Alamofire").click();
-#         self.driver.find_element(By.ID,"put").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the DELETE network request with the Alamofire and Validating the network span data.
-#     '''   
-#     def test_Alamofire_DeleteClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"Alamofire").click();
-#         self.driver.find_element(By.ID,"delete").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the POST network request with the AFNetworking and Validating the network span data.
-#     ''' 
-#     def test_AFNetworking_PostClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"AFNetworking").click();
-#         self.driver.find_element(By.ID,"post").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the GET network request with the AFNetworking and Validating the network span data.
-#     '''   
-#     def test_AFNetworking_GetClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"AFNetworking").click();
-#         self.driver.find_element(By.ID,"get").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Generating the PUT network request with the AFNetworking and Validating the network span data.
-#     '''     
-#     def test_AFNetworking_PutClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"AFNetworking").click();
-#         self.driver.find_element(By.ID,"put").click();
-#         self.validate_span();
-
-#     ''' 
-#     Generating the DELETE network request with the AFNetworking and Validating the network span data.
-#     '''     
-#     def test_AFNetworking_DeleteClick(self):
-#         self.driver.find_element(By.ID,"Network Request").click();
-#         self.driver.find_element(By.ID,"AFNetworking").click();
-#         self.driver.find_element(By.ID,"delete").click();
-#         self.validate_span();
-
-#     ''' 
-#     Generating the screen navigation data and Validating span data.
-#     '''    
-#     def test_ScreenTrackClick(self):
-#         self.driver.find_element(By.ID,"Screen-Track").click();
-#         self.driver.find_element(By.ID,"Custom Screen Name").click();
-#         self.validate_span();
-        
-#     ''' 
-#     Validating custom span.
-#     '''
-#     def test_CustomSpanClick(self):
-#         self.driver.find_element(By.ID,"Custom").click()
-#         sleep(5)
-#         self.driver.find_element(By.ID,"SDK Initialize validation").click()
-#         WebDriverWait(self.driver, 5).until(
-#             EC.element_to_be_clickable((By.ID, "Success")),
-#             message='Span validation failed',
-#         )  
-        
-#     ''' 
-#     Validating Error/Exception span.
-#     '''
-#     def test_ErrorSpanClick(self):
-#         self.driver.find_element(By.ID,"Error").click()
-#         sleep(5)
-#         self.driver.find_element(By.ID,"SDK Initialize validation").click()
-#         WebDriverWait(self.driver, 5).until(
-#             EC.element_to_be_clickable((By.ID, "Success")),
-#             message='Span validation failed',
-#         )
+    ''' 
+    Generating the POST network request with the URLSession and Validating the network span data.
+    '''
+    def test_API_PostClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"URLSession").click();
+        self.driver.find_element(By.ID,"post").click();
+        self.validate_span();
 
     ''' 
-    Validating resignactive span.
+    Generating the GET network request with the URLSession and Validating the network span data.
     '''
-    def test_ResignActiveSpanClick(self):
-        self.driver.find_element(By.ID,"BackGround/ForeGround").click()
-        self.driver.background_app(10)
-        self.driver.activate_app(BUNDLE_ID)
+    def test_API_GetClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"URLSession").click();
+        self.driver.find_element(By.ID,"get").click();
+        self.validate_span();
+
+    ''' 
+    Generating the PUT network request with the URLSession and Validating the network span data.
+    '''
+    def test_API_PutClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"URLSession").click();
+        self.driver.find_element(By.ID,"put").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the DELETE network request with the URLSession and Validating the network span data.
+    '''
+    def test_API_DeleteClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"URLSession").click();
+        self.driver.find_element(By.ID,"delete").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the POST network request with the Alamofire and Validating the network span data.
+    '''
+    def test_Alamofire_PostClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"Alamofire").click();
+        self.driver.find_element(By.ID,"post").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the GET network request with the Alamofire and Validating the network span data.
+    '''    
+    def test_Alamofire_GetClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"Alamofire").click();
+        self.driver.find_element(By.ID,"get").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the PUT network request with the Alamofire and Validating the network span data.
+    '''
+    def test_Alamofire_PutClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"Alamofire").click();
+        self.driver.find_element(By.ID,"put").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the DELETE network request with the Alamofire and Validating the network span data.
+    '''   
+    def test_Alamofire_DeleteClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"Alamofire").click();
+        self.driver.find_element(By.ID,"delete").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the POST network request with the AFNetworking and Validating the network span data.
+    ''' 
+    def test_AFNetworking_PostClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"AFNetworking").click();
+        self.driver.find_element(By.ID,"post").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the GET network request with the AFNetworking and Validating the network span data.
+    '''   
+    def test_AFNetworking_GetClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"AFNetworking").click();
+        self.driver.find_element(By.ID,"get").click();
+        self.validate_span();
+        
+    ''' 
+    Generating the PUT network request with the AFNetworking and Validating the network span data.
+    '''     
+    def test_AFNetworking_PutClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"AFNetworking").click();
+        self.driver.find_element(By.ID,"put").click();
+        self.validate_span();
+
+    ''' 
+    Generating the DELETE network request with the AFNetworking and Validating the network span data.
+    '''     
+    def test_AFNetworking_DeleteClick(self):
+        self.driver.find_element(By.ID,"Network Request").click();
+        self.driver.find_element(By.ID,"AFNetworking").click();
+        self.driver.find_element(By.ID,"delete").click();
+        self.validate_span();
+
+    ''' 
+    Generating the screen navigation data and Validating span data.
+    '''    
+    def test_ScreenTrackClick(self):
+        self.driver.find_element(By.ID,"Screen-Track").click();
+        self.driver.find_element(By.ID,"Custom Screen Name").click();
+        self.validate_span();
+        
+    ''' 
+    Validating custom span.
+    '''
+    def test_CustomSpanClick(self):
+        self.driver.find_element(By.ID,"Custom").click()
+        sleep(5)
+        self.driver.find_element(By.ID,"SDK Initialize validation").click()
+        WebDriverWait(self.driver, 5).until(
+            EC.element_to_be_clickable((By.ID, "Success")),
+            message='Span validation failed',
+        )  
+        
+    ''' 
+    Validating Error/Exception span.
+    '''
+    def test_ErrorSpanClick(self):
+        self.driver.find_element(By.ID,"Error").click()
         sleep(5)
         self.driver.find_element(By.ID,"SDK Initialize validation").click()
         WebDriverWait(self.driver, 5).until(
             EC.element_to_be_clickable((By.ID, "Success")),
             message='Span validation failed',
         )
+
+#     ''' 
+#     Validating resignactive span.
+#     '''
+#     def test_ResignActiveSpanClick(self):
+#         self.driver.find_element(By.ID,"BackGround/ForeGround").click()
+#         self.driver.background_app(10)
+#         self.driver.activate_app(BUNDLE_ID)
+#         sleep(5)
+#         self.driver.find_element(By.ID,"SDK Initialize validation").click()
+#         WebDriverWait(self.driver, 5).until(
+#             EC.element_to_be_clickable((By.ID, "Success")),
+#             message='Span validation failed',
+#         )
 
 #     ''' 
 #     Validating enterforeground span.
@@ -234,31 +234,31 @@ class IOSTests(unittest.TestCase):
 #             message='Span validation failed',
 #         )
 
-#     ''' 
-#     Validating AppTerminate span.
-#     '''
-#     def test_AppTerminateSpanClick(self):
-#         self.driver.find_element(By.ID,"Terminate").click()
-#         sleep(5)
-#         self.driver.find_element(By.ID,"SDK Initialize validation").click()
-#         WebDriverWait(self.driver, 5).until(
-#             EC.element_to_be_clickable((By.ID, "Success")),
-#             message='Span validation failed',
-#         )
+    ''' 
+    Validating AppTerminate span.
+    '''
+    def test_AppTerminateSpanClick(self):
+        self.driver.find_element(By.ID,"Terminate").click()
+        sleep(5)
+        self.driver.find_element(By.ID,"SDK Initialize validation").click()
+        WebDriverWait(self.driver, 5).until(
+            EC.element_to_be_clickable((By.ID, "Success")),
+            message='Span validation failed',
+        )
         
-#     ''' 
-#     Validating crash span.
-#     '''
-#     def test_CrashOnViewLoadClick(self):
-#         self.driver.find_element(By.ID,"Crash").click();
-#         self.driver.find_element(By.ID,"Crash on ViewDidload").click();
-#         self.driver.activate_app(BUNDLE_ID)
-#         sleep(5)
-#         self.driver.find_element(By.ID,"SDK Initialize validation").click()
-#         WebDriverWait(self.driver, 5).until(
-#             EC.element_to_be_clickable((By.ID, "Success")),
-#             message='Span validation failed',
-#         )
+    ''' 
+    Validating crash span.
+    '''
+    def test_CrashOnViewLoadClick(self):
+        self.driver.find_element(By.ID,"Crash").click();
+        self.driver.find_element(By.ID,"Crash on ViewDidload").click();
+        self.driver.activate_app(BUNDLE_ID)
+        sleep(5)
+        self.driver.find_element(By.ID,"SDK Initialize validation").click()
+        WebDriverWait(self.driver, 5).until(
+            EC.element_to_be_clickable((By.ID, "Success")),
+            message='Span validation failed',
+        )
 
      
     def validate_span(self):
