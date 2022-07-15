521e20426a1a94095dfe5973c24a7d755b0ed0fa
shattimare <98871176+shattimare@users.noreply.github.com>
2022-07-15 16:14:25 +0530

    Update test_appium.py

--- SplunkRumWorkspace/SmokeTest/test_appium.py
+++ SplunkRumWorkspace/SmokeTest/test_appium.py
@@ -28,7 +28,7 @@ class IOSTests(unittest.TestCase):
         #caps['sauce:options']['appiumVersion'] = '1.22.3'
         caps['sauce:options']['build'] = 'Platform Configurator Build ' + currentDate
         caps['sauce:options']['name'] = 'Platform Configurator Job ' + currentTime
-        caps['USE_PREBUILT_WDA'] = true
+        caps['USE_PREBUILT_WDA'] = 'true'
         
         url = 'https://sso-splunk.saucelabs.com-shattimare:aee7320d-0d97-469d-a6a4-3d4c1ed6c5f0@ondemand.us-west-1.saucelabs.com:443/wd/hub'
         self.driver=webdriver.Remote(url,caps)
