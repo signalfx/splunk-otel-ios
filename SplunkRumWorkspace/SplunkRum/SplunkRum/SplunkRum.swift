d42fe2f645c7c629d69d92a2107b7d66367900de
Piyush23786 <piyushp@splunk.com>
2022-08-11 20:56:22 +0530

    APMI-2306 slow and frozen frame detection
    Changes: with signed commits.
    
    Pause CADisplayLink when on app will resign and resume it on app activation. Simplifies the code somewhat and avoids issues with counting the frame duration when the app is moved to background.
    Group slow and frozen frames per screen names. This is necessary so when 2 different screens have a slow frame in a second interval, they are both reported.
    Use screen name callbacks to store the screen name locally to avoid the locking in getScreenName()
    Use a 1 second timer to periodically dump the detected frames.

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -46,7 +46,9 @@ public let DEFAULT_DISK_CACHE_MAX_SIZE_BYTES: Int64 = 25 * 1024 * 1024
                       screenNameSpans: Bool = true,
                       networkInstrumentation: Bool = true,
                       enableDiskCache: Bool = false,
-                      spanDiskCacheMaxSize: Int64 = DEFAULT_DISK_CACHE_MAX_SIZE_BYTES
+                      spanDiskCacheMaxSize: Int64 = DEFAULT_DISK_CACHE_MAX_SIZE_BYTES,
+                      slowFrameDetectionThresholdMs: Double = 16.7,
+                      frozenFrameDetectionThresholdMs: Double = 700
     ) {
         // rejectionFilter not specified to make it possible to call from objc
         self.allowInsecureBeacon = allowInsecureBeacon
@@ -58,6 +60,8 @@ public let DEFAULT_DISK_CACHE_MAX_SIZE_BYTES: Int64 = 25 * 1024 * 1024
         self.networkInstrumentation = networkInstrumentation
         self.enableDiskCache = enableDiskCache
         self.spanDiskCacheMaxSize = spanDiskCacheMaxSize
+        self.slowFrameDetectionThresholdMs = slowFrameDetectionThresholdMs
+        self.frozenFrameDetectionThresholdMs = frozenFrameDetectionThresholdMs
     }
     /**
         Copy constructor
@@ -72,6 +76,8 @@ public let DEFAULT_DISK_CACHE_MAX_SIZE_BYTES: Int64 = 25 * 1024 * 1024
         self.spanFilter = opts.spanFilter
         self.showVCInstrumentation = opts.showVCInstrumentation
         self.screenNameSpans = opts.screenNameSpans
+        self.slowFrameDetectionThresholdMs = opts.slowFrameDetectionThresholdMs
+        self.frozenFrameDetectionThresholdMs = opts.frozenFrameDetectionThresholdMs
         self.networkInstrumentation = opts.networkInstrumentation
         self.enableDiskCache = opts.enableDiskCache
         self.spanDiskCacheMaxSize = opts.spanDiskCacheMaxSize
@@ -119,6 +125,16 @@ public let DEFAULT_DISK_CACHE_MAX_SIZE_BYTES: Int64 = 25 * 1024 * 1024
      Enable NetworkInstrumentation span creation for https calls.
      */
     @objc public var networkInstrumentation: Bool = true
+
+    /**
+     Threshold, in milliseconds, from which to count a rendered frame as slow.
+    */
+    @objc public var slowFrameDetectionThresholdMs: Double = 16.7
+
+    /**
+     Threshold, in milliseconds, from which to count a rendered frame as frozen.
+    */
+    @objc public var frozenFrameDetectionThresholdMs: Double = 700
 
     /**
      Enable caching created spans to disk. On successful exports the spans are deleted.
@@ -244,6 +260,10 @@ var splunkRumInitializeCalledTime = Date()
         }
         initializeNetworkTypeMonitoring()
         initalizeUIInstrumentation()
+        startSlowFrameDetector(
+                    slowFrameDetectionThresholdMs: options?.slowFrameDetectionThresholdMs,
+                    frozenFrameDetectionThresholdMs: options?.frozenFrameDetectionThresholdMs
+                )
         // not initializeAppLifecycleInstrumentation, done at end of AppStart
         srInit.end()
         initialized = true
