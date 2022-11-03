702fcdf0d0d86823a345c0fb4ac545d4a0732864
Piyush Patil <piyushp@splunk.com>
2022-11-03 17:19:25 +0530

    Fix-switching to a wifi network, the carrier info should be cleared.
    switching from wifi to cellular and back again while the app is running

--- SplunkRumWorkspace/SplunkRum/SplunkRum/NetworkType.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/NetworkType.swift
@@ -153,9 +153,9 @@ func initializeNetworkTypeMonitoring() {
             } else {
                 setConnectionType(nil)
             }
+            setCarrierInfo(telephonyNetworkInfo, identifier: telephonyNetworkInfo.serviceCurrentRadioAccessTechnology?.keys.first)
         }
 
-        setCarrierInfo(telephonyNetworkInfo, identifier: telephonyNetworkInfo.serviceCurrentRadioAccessTechnology?.keys.first)
         telephonyNetworkInfo.serviceSubscriberCellularProvidersDidUpdateNotifier = { identifier in
             setCarrierInfo(telephonyNetworkInfo, identifier: identifier)
         }
