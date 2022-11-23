9227727855f4bde35d124d8dca37a28e02607ea2
Juan Nino <jnino@splunk.com>
2022-11-23 13:46:39 -0700

    MOB-2771 - swiftlint orphaned_doc_comment

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -188,6 +188,7 @@ var splunkRumInitializeCalledTime = Date()
     static var configuredOptions: SplunkRumOptions?
     static var theBeaconUrl: String?
 
+    // swiftlint:disable:next orphaned_doc_comment
     /**
             Initialization function.  Call as early as possible in your application, but only on the main thread.
                 - Parameter beaconUrl: Destination for the captured data.
