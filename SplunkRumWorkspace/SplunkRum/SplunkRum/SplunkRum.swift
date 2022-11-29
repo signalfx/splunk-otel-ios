8423082b189fe0d4f1e47e869b1450ca2c9f861c
Juan Nino <jnino@splunk.com>
2022-11-29 07:43:41 -0700

    SplunkRum.initialize discardable result

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SplunkRum.swift
@@ -196,6 +196,7 @@ var splunkRumInitializeCalledTime = Date()
                 - Parameter options: Non-required configuration toggles for various features.  See SplunkRumOptions struct for details.
      
      */
+    @discardableResult
     @objc public class func initialize(beaconUrl: String, rumAuth: String, options: SplunkRumOptions? = nil) -> Bool {
         if !Thread.isMainThread {
             print("SplunkRum: Please call SplunkRum.initialize only on the main thread")
