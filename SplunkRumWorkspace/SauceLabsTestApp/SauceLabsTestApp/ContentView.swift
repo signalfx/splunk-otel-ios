49d29825518f415c895271bb388167202f646e87
Siim Kallas <skallas@splunk.com>
2023-04-16 23:51:57 +0300

    feat: add ci script

---
+++ SplunkRumWorkspace/SauceLabsTestApp/SauceLabsTestApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SauceLabsTestApp
+//
+//  Created by Siim Kallas on 13/04/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplunkRumWorkspace/SauceLabsTestApp/SauceLabsTestApp/SauceLabsTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SauceLabsTestAppApp.swift
+//  SauceLabsTestApp
+//
+//  Created by Siim Kallas on 13/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SauceLabsTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
