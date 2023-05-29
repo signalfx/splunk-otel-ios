0313735b5e6d7ce4d490c49d6d6b88805865ebb4
Siim Kallas <skallas@splunk.com>
2023-05-30 00:41:39 +0300

    update sl script

--- SplunkRumWorkspace/SauceLabsTestApp/sl_tests.py
+++ SplunkRumWorkspace/SauceLabsTestApp/sl_tests.py
@@ -1,22 +1,15 @@
 import unittest
 import os
 from appium import webdriver
-from time import sleep
-from random import choice, randint
 from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.common.exceptions import NoSuchElementException
-from selenium.common.exceptions import TimeoutException
 import sys
-
-BUNDLE_ID = 'com.splunk.opentelemetry.SmokeTest'
 
 class IOSTests(unittest.TestCase):
 
     def setUp(self):
-        currentDate = datetime.now().strftime('%Y-%m-%d')
         currentTime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
         caps = {}
 
@@ -28,8 +21,7 @@ class IOSTests(unittest.TestCase):
         caps['appium:platformVersion'] = sys.argv[1]
         caps['appium:automationName'] = 'XCUITest'
         caps['sauce:options'] = {}
-        caps['sauce:options']['build'] = 'Platform Configurator Build ' + currentDate
-        caps['sauce:options']['name'] = 'Platform Configurator Job ' + currentTime
+        caps['sauce:options']['name'] = 'SplunkRum tests' + currentTime
    
         sl_user = os.environ['SAUCELABS_USER']
         sl_key = os.environ['SAUCELABS_KEY']
@@ -39,41 +31,12 @@ class IOSTests(unittest.TestCase):
     def tearDown(self):
         self.driver.quit()
 
-    def test_network_instrumentation(self):
-        self.driver.find_element(By.ID, "networkInstrTests").click()
-        self.driver.find_element(By.ID, "urlSessionTests").click()
+    def test_spans(self):
+        self.driver.find_element(By.ID, "results").click()
         WebDriverWait(self.driver, 10).until(
             EC.text_to_be_present_in_element((By.ID, "testResult"), "success")
         )
 
-     
-    def validate_span(self):
-        sleep(10);  #it takes time to generate spans.
-        self.driver.find_element(By.ID,"Span Validation").click();
-        try:
-            WebDriverWait(self.driver, 10,5,NoSuchElementException).until(
-                EC.visibility_of_element_located((By.ID, "Success")),
-                message='Span validation failed',
-            )
-        except TimeoutException:
-                self.driver.find_element(By.ID,"Success")
-
-        
-    def validate_more_spans(self):
-        sleep(10) #it takes time to generate spans.
-        self.driver.execute_script('mobile: scroll', {'direction': 'down'})
-        el=self.driver.find_element(By.ID,"Span Validation")
-        el.click()
-        try:
-            WebDriverWait(self.driver, 10,5,NoSuchElementException).until(
-                EC.visibility_of_element_located((By.ID, "Success")),
-                message='Span validation failed',
-            )
-        except TimeoutException:
-                self.driver.find_element(By.ID,"Success")
-   
-        
-   
 if __name__ == "__main__":
     suite = unittest.TestLoader().loadTestsFromTestCase(IOSTests)
     testRunner_result = unittest.TextTestRunner(verbosity=2).run(suite)
