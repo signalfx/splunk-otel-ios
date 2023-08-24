04128c02927de0fd0a9c4684b3eb959262c83ea7
Ruswan Efendi <ruswan.wu@outlook.com>
2023-08-24 21:29:01 +0700

    Set sampler only once to prevent crashes

--- SplunkRumWorkspace/SplunkRum/SplunkRum/SessionBasedSampler.swift
+++ SplunkRumWorkspace/SplunkRum/SplunkRum/SessionBasedSampler.swift
@@ -16,40 +16,66 @@ limitations under the License.
 
 import Foundation
 
-class SessionBasedSampler {
-    static var probability: Double = 1.0
-    private let sessionIdCallback: (() -> Void) = {
-        sessionShouldSample()
-    }
-
+class SessionBasedSampler: Sampler {
+    
+    var probability: Double = 1.0
+    var currentlySampled: Bool?
+    var lock: Lock = Lock()
+    
     init(ratio: Double) {
-        SessionBasedSampler.probability = ratio
-        addSessionIdCallback(sessionIdCallback)
+        probability = ratio
+        observeSessionIdChange()
     }
-
+    
+    func shouldSample(parentContext: SpanContext?, traceId: TraceId, name: String, kind: SpanKind, attributes: [String : AttributeValue], parentLinks: [SpanData.Link]) -> Decision {
+        return lock.withLock({
+            return self.getDecision()
+        })
+        
+    }
+    
+    var description: String {
+        return "SessionBasedSampler, Ratio: \(probability)"
+    }
+    
+    private func observeSessionIdChange() {
+        addSessionIdCallback { [weak self] in
+            self?.lock.withLockVoid {
+                self?.currentlySampled = self?.shouldSampleNewSession()
+            }
+        }
+    }
+    
+    private func getDecision() -> Decision {
+        
+        if let currentlySampled = self.currentlySampled {
+            return self.getDecision(shouldSample: currentlySampled)
+        }
+        
+        let newShouldSample = self.shouldSampleNewSession()
+        self.currentlySampled = newShouldSample
+        return self.getDecision(shouldSample: newShouldSample)
+    }
+    
+    private func getDecision(shouldSample: Bool) -> Decision {
+        return shouldSample ? Samplers.alwaysOnDecision : Samplers.alwaysOffDecision
+    }
+    
     /**Check if session will be sampled or not.**/
-    @discardableResult public class func sessionShouldSample() -> Bool {
-
+    private func shouldSampleNewSession() -> Bool {
+        
         var result = false
-        switch SessionBasedSampler.probability {
+        
+        switch probability {
         case 0.0:
             result = false
         case 1.0:
             result = true
         default:
-            result = Double.random(in: 0.0...1.0) <= SessionBasedSampler.probability
+            result = Double.random(in: 0.0...1.0) <= probability
         }
-
-        var parentSampler: Sampler
-        if result {
-            parentSampler = Samplers.parentBased(root: Samplers.alwaysOn, remoteParentSampled: Samplers.alwaysOn, remoteParentNotSampled: Samplers.alwaysOn, localParentSampled: Samplers.alwaysOn, localParentNotSampled: Samplers.alwaysOn)
-        } else {
-            parentSampler = Samplers.parentBased(root: Samplers.alwaysOff, remoteParentSampled: Samplers.alwaysOff, remoteParentNotSampled: Samplers.alwaysOff, localParentSampled: Samplers.alwaysOff, localParentNotSampled: Samplers.alwaysOff)
-        }
-
-        let tracerProvider = OpenTelemetry.instance.tracerProvider as! TracerProviderSdk
-        tracerProvider.updateActiveSampler(parentSampler)
+        
         return result
     }
-
+    
 }
