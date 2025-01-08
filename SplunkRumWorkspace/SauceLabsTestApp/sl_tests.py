09926754bb7f8459e16a9f696d5aedffc8069f59
Aditi Sharma <aditis3@cisco.com>
2025-01-07 16:49:43 -0800

    Update remote call for saucelabs

--- SplunkRumWorkspace/SauceLabsTestApp/sl_tests.py
+++ SplunkRumWorkspace/SauceLabsTestApp/sl_tests.py
@@ -1,6 +1,6 @@
 import unittest
 import os
-from appium import webdriver
+from appium import webdriver as appiumdriver
 from datetime import datetime
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -25,7 +25,7 @@ class IOSTests(unittest.TestCase):
         caps['sauce:options']['accessKey'] = os.environ['SAUCE_ACCESS_KEY']
         caps['sauce:options']['username'] = os.environ['SAUCE_USERNAME']
         url = f'https://ondemand.us-west-1.saucelabs.com:443/wd/hub'
-        self.driver=webdriver.Remote(url, caps)
+        self.driver=appiumdriver.Remote(url, desired_capabilities=caps)
 
         
     def tearDown(self):
