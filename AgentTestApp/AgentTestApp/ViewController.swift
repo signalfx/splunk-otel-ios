367d276e46e0ea02e32cca243720260bbfbbf2f3
Carlos McEvilly <12955+carlosmcevilly@users.noreply.github.com>
2025-01-21 13:28:50 -0800

    DEMRUM-620: Add exercise of slow frame detection to test app (#236)
    * DEMRUM-620: Add exercise of slow frame detection to test app
    
    * DEMRUM-620: swiftlint: fix trailing spaces
    
    * DEMRUM-620: fix typos
    
    * DEMRUM-620: swiftlint: remove weak from IBOutlet
    
    * DEMRUM-620: swiftlint: remove needless self references and fix whitespace
    
    * DEMRUM-620: swiftlint: sort imports alphabetically
    
    * DEMRUM-620: swiftlint: remove weak from IBOutlet

--- AgentTestApp/AgentTestApp/ViewController.swift
+++ AgentTestApp/AgentTestApp/ViewController.swift
@@ -7,51 +7,52 @@
 
 import UIKit
 
-class ViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
-    // Data model: These strings will be the data for the table view cells and drive the seque
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+
+    // Data model: These strings will be the data for the table view cells and drive the segue
     //             Each array must have the same number of elements
-    let displayName: [String] = ["Network Calls", "Crashes", "Test API Sample Calls", "Sample Next Task"]        // Can be any descriptive text
-    let segueName: [String] = ["NetworkCalls", "Crashes", "TestApiCalls", "PlaceHolder"]               // Segues declared in the storyboard
-    
+    let displayName: [String] = ["Network Calls", "Crashes", "Test API Sample Calls", "Sample Next Task"] // Can be any descriptive text
+    let segueName: [String] = ["NetworkCalls", "Crashes", "TestApiCalls", "SlowFrameRenders", "PlaceHolder"] //  Segues declared in the storyboard
+
     // cell reuse id (cells that scroll out of view can be reused)
     let cellReuseIdentifier = "cell"
 
     // don't forget to hook this up from the storyboard
-    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Register the table view cell class and its reuse id
-        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
-        
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellReuseIdentifier)
+
         // This view controller itself will provide the delegate methods and row data for the table view.
         tableView.delegate = self
         tableView.dataSource = self
     }
-    
+
     // number of rows in table view
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.displayName.count
+        return displayName.count
     }
-    
+
     // create a cell for each table view row
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        
+
         // create a new cell if needed or reuse an old one
-        let cell:UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
-        
+        let cell: UITableViewCell = (self.tableView.dequeueReusableCell(withIdentifier: cellReuseIdentifier) as UITableViewCell?)!
+
         // set the text from the data model
-        cell.textLabel?.text = self.displayName[indexPath.row]
-        
+        cell.textLabel?.text = displayName[indexPath.row]
+
         return cell
     }
-    
+
     // method to run when table view cell is tapped
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("You tapped row \(indexPath.row), for \(self.displayName[indexPath.row])")
-        
-        self.performSegue(withIdentifier: self.segueName[indexPath.row], sender: self)
+        print("You tapped row \(indexPath.row), for \(displayName[indexPath.row])")
+
+        performSegue(withIdentifier: segueName[indexPath.row], sender: self)
     }
 }
+
