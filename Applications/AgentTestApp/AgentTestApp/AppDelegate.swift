aac2638817b7b541e3f0c6b2c54828ab00b01afe
Václav Halík <vhalik@splunk.com>
2025-04-09 14:42:48 +0200

    I have read the CLA Document and I hereby sign the CLA

--- Applications/AgentTestApp/AgentTestApp/AppDelegate.swift
+++ Applications/AgentTestApp/AgentTestApp/AppDelegate.swift
@@ -20,7 +20,6 @@ import SplunkAgent
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -42,6 +41,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         return true
     }
+
 
     // MARK: UISceneSession Lifecycle
 
