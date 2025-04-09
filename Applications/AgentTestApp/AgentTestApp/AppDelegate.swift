5c476ca5a7840ba9098a204998204fac1f644c1d
Václav Halík <vhalik@splunk.com>
2025-04-09 14:46:28 +0200

    DEMRUM-1145 - Remove the empty line.

--- Applications/AgentTestApp/AgentTestApp/AppDelegate.swift
+++ Applications/AgentTestApp/AgentTestApp/AppDelegate.swift
@@ -21,6 +21,7 @@ import SplunkAgent
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
