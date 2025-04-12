74ef72499c41c0140adcd9d87a56ee1a7badd349
Carlos McEvilly <cmcevill@cisco.com>
2025-04-11 18:48:05 -0700

    DEMRUM-861: eliminate AttributeContainer and just extend Dictionary

--- SplunkCustomTracking/Sources/SplunkCustomTracking/DataTypes/DataHelpers/MutableAttributes.swift
+++ SplunkCustomTracking/Sources/SplunkCustomTracking/DataTypes/DataHelpers/MutableAttributes.swift
@@ -18,124 +18,30 @@ limitations under the License.
 import Foundation
 
 
-// MARK: AttributeContainer: Protocol for common attribute operations
-protocol AttributeContainer {
+extension Dictionary where Key == String, Value == EventAttributeValue {
 
-    // MARK: - Protocol required functions
-
-    // Stores EventAttributeValue attributes in a key-value format
-    var attributes: [String: EventAttributeValue] { get set }
-
-    // Adds an attribute or updates an existing one
-    mutating func setAttribute(for key: String, value: EventAttributeValue)
-
-    // Retrieves a specific attribute
-    func getAttribute(for key: String) -> EventAttributeValue?
-
-    // Returns all attributes to support operations like data output
-    func listAttributes() -> [String: EventAttributeValue]
-
-    // Removes a specific attribute
-    mutating func remove(key: String)
-
-    // Clears all attributes to reset the storage
-    mutating func removeAll()
-
-    // Replaces existing attributes with new ones for batch updates
-    mutating func setAll(newAttributes: [String: EventAttributeValue])
-
-    // Applies a transformation using the Mapping pattern
-    func apply<U: Hashable>(mappingClosure: (String, EventAttributeValue) -> U) -> MutableAttributes
-
-    // Modifies attributes in place using the Mutator pattern
-    mutating func apply(mutatingClosure: (String, inout EventAttributeValue) -> Void)
-}
-
-
-// MARK: - AttributeContainer: Default implementations for AttributeContainer
-extension AttributeContainer {
-
-    // MARK: - Protocol required function default implementations
-
-    func getAttribute(for key: String) -> EventAttributeValue? {
-        return attributes[key]
-    }
-
-    func listAttributes() -> [String: EventAttributeValue] {
-        return attributes
-    }
-
-    mutating func remove(key: String) {
-        attributes[key] = nil
-    }
-
-    mutating func removeAll() {
-        attributes.removeAll()
-    }
-
-    mutating func setAll(newAttributes: [String: EventAttributeValue]) {
-        attributes = newAttributes
-    }
-
-    // MARK: - Mapping pattern to return transformed attributes
-
-    func apply<U: Hashable>(mappingClosure: (String, EventAttributeValue) -> U) -> MutableAttributes {
-        var mappedAttributes = MutableAttributes()
-        for (key, value) in listAttributes() {
-            let _ = mappingClosure(key, value)
-            mappedAttributes.setAttribute(for: key, value: value)
-        }
-        return mappedAttributes
-    }
-
-    // MARK: - Mutator Pattern: Modify attributes in place
-
-    mutating func apply(mutatingClosure: (String, inout EventAttributeValue) -> Void) {
-        for (key, var value) in attributes {
-            mutatingClosure(key, &value)
-            attributes[key] = value
-        }
-    }
-}
-
-
-// MARK: - MutableAttributes: Base struct for managing attributes
-struct MutableAttributes: AttributeContainer {
-    var attributes: [String: EventAttributeValue] = [:]
-
-    mutating func setAttribute(for key: String, value: EventAttributeValue) {
-        attributes[key] = value
-    }
-}
-
-
-// MARK: - LengthEnforcable: Protocol for enforcing length constraints
-protocol LengthEnforcable: AttributeContainer {
-    var maxKeyLength: Int { get }
-    var maxValueLength: Int { get }
-    mutating func setAttribute(for key: String, value: EventAttributeValue) -> Bool
-}
-
-
-// MARK: - LengthEnforcable: Extension to provide default length constraints
-extension LengthEnforcable {
-    var maxKeyLength: Int { 1024 }
-    var maxValueLength: Int { 2048 }
-}
-
-
-// MARK: - ConstrainedAttributes: Struct for managing attributes with length constraints
-struct ConstrainedAttributes: LengthEnforcable {
-    var attributes: [String: EventAttributeValue] = [:]
-
-    // MARK: - Constrained setAttribute that checks the length of its keys and values
-
-    mutating func setAttribute(for key: String, value: EventAttributeValue) -> Bool {
+    mutating func setAttribute(for key: String, value: EventAttributeValue, maxKeyLength: Int = 1024, maxValueLength: Int = 2048) -> Bool {
         guard key.count <= maxKeyLength, value.description.count <= maxValueLength else {
             print("Invalid key or value length.")
             return false
         }
-        attributes[key] = value
+        self[key] = value
         return true
+    }
+
+    func apply<U>(mappingClosure: (String, EventAttributeValue) -> U) -> [String: U] {
+        var mappedAttributes: [String: U] = [:]
+        for (key, value) in self {
+            let newValue = mappingClosure(key, value)
+            mappedAttributes[key] = newValue
+        }
+        return mappedAttributes
+    }
+
+    mutating func apply(mutatingClosure: (String, inout EventAttributeValue) -> Void) {
+        for (key, var value) in self {
+            mutatingClosure(key, &value)
+            self[key] = value
+        }
     }
 }
