ba2970ec0c1276591920d85631bbfbc5a5a46656
Carlos McEvilly <cmcevill@cisco.com>
2025-04-14 23:52:22 -0700

    DEMRUM-861: Make validation code more DRY with an inner function

--- SplunkCustomTracking/Sources/SplunkCustomTracking/DataTypes/DataHelpers/ValidationUtils.swift
+++ SplunkCustomTracking/Sources/SplunkCustomTracking/DataTypes/DataHelpers/ValidationUtils.swift
@@ -22,25 +22,24 @@ import SplunkLogger
 let maxKeyLength = 1024
 let maxValueLength = 2048
 
-/// Validates the lengths of keys and values in a dictionary of attributes.
-/// Logs a warning message if any attribute exceeds the maximum length.
-/// - Parameters:
-///   - attributes: The dictionary of attributes to validate.
-///   - logger: The logger to use for warning messages.
-/// - Returns: A boolean indicating whether all attributes are valid.
+// MARK: - validateAttributeLengths
+
+// Validate the lengths of keys and values in a dictionary of attributes.
 func validateAttributeLengths(attributes: [String: EventAttributeValue], logger: InternalLogger) -> Bool {
+
+    func logAndReturnFalse(type: String, key: String) -> Bool {
+        logger.log(level: .warning) {
+            "Invalid \(type) length for key '\(key)'. Not publishing this event."
+        }
+        return false
+    }
+
     for (key, value) in attributes {
         if key.count > maxKeyLength {
-            logger.log(level: .warning) {
-                "Invalid key length for key '\(key)'. Not publishing this event."
-            }
-            return false
+            return logAndReturnFalse(type: "key", key: key)
         }
         if let stringValue = value as? String, stringValue.count > maxValueLength {
-            logger.log(level: .warning) {
-                "Invalid value length for key '\(key)'. Not publishing this event."
-            }
-            return false
+            return logAndReturnFalse(type: "value", key: key)
         }
     }
     return true
