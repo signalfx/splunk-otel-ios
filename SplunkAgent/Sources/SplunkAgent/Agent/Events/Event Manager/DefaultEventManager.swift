22d969d6a9505ea975d140c5c6b3e18d9c4e96d6
Jiri Charvat <jcharvat@splunk.com>
2025-04-28 11:00:25 +0200

    Update DefaultEventManager.

--- SplunkAgent/Sources/SplunkAgent/Agent/Events/Event Manager/DefaultEventManager.swift
+++ SplunkAgent/Sources/SplunkAgent/Agent/Events/Event Manager/DefaultEventManager.swift
@@ -84,21 +84,46 @@ class DefaultEventManager: AgentEventManager {
             osType: SystemInfo.type
         )
 
-        // Initialize log event processor
-        logEventProcessor = OTLPLogEventProcessor(
-            with: configuration.logsUrl,
-            resources: resources,
-            runtimeAttributes: agent.runtimeAttributes,
-            debugEnabled: configuration.enableDebugLogging
-        )
+        // Initialize log event processor. ‼️ Using trace endpoint as a placeholder
+        if let logUrl = configuration.endpoint.traceEndpoint {
+            logEventProcessor = OTLPLogEventProcessor(
+                with: logUrl,
+                resources: resources,
+                runtimeAttributes: agent.runtimeAttributes,
+                debugEnabled: configuration.enableDebugLogging
+            )
+
+            logger.log(level: .info) {
+                "Using log url: \(logUrl)"
+            }
+        } else {
+            logEventProcessor = NoopLogEventProcessor()
+
+            logger.log(level: .error) {
+                "Missing log endpoint URL. Logs will not be sent."
+            }
+        }
 
         // Initialize trace processor
-        traceProcessor = OTLPTraceProcessor(
-            with: configuration.tracesUrl,
-            resources: resources,
-            runtimeAttributes: agent.runtimeAttributes,
-            debugEnabled: configuration.enableDebugLogging
-        )
+        if let traceUrl = configuration.endpoint.traceEndpoint {
+            traceProcessor = OTLPTraceProcessor(
+                with: traceUrl,
+                resources: resources,
+                runtimeAttributes: agent.runtimeAttributes,
+                debugEnabled: configuration.enableDebugLogging
+            )
+
+            logger.log(level: .info) {
+                "Using trace url: \(traceUrl)"
+            }
+
+        } else {
+            traceProcessor = NoopTraceProcessor()
+
+            logger.log(level: .error) {
+                "Missing trace endpoint URL. Traces will not be sent."
+            }
+        }
 
         // Starts listening to a Session Reset notification to send the Session Start event.
         NotificationCenter.default.addObserver(forName: DefaultSession.sessionDidResetNotification, object: nil, queue: nil) { _ in
