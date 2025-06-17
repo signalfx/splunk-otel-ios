3c9f279e58aca80b3ee5c0071c90d97536090fec
Filip Ruzicka <ruzicka@cisco.com>
2025-06-17 13:29:53 +0200

    fix comments vrom codereview

--- SplunkAgent/Sources/SplunkAgent/Public API/Modules/Interactions/Proxies/InteractionsModule.swift
+++ SplunkAgent/Sources/SplunkAgent/Public API/Modules/Interactions/Proxies/InteractionsModule.swift
@@ -16,7 +16,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
 import Foundation
 
 /// The InteractionsModule handles user interactions within the app.
--- SplunkAgent/Sources/SplunkAgent/Public API/Modules/Interactions/Proxies/NonOperational/InteractionsNonOperational.swift
+++ SplunkAgent/Sources/SplunkAgent/Public API/Modules/Interactions/Proxies/NonOperational/InteractionsNonOperational.swift
@@ -15,7 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
 final class InteractionsNonOperational: InteractionsModule {
 
     // MARK: - Custom ids
