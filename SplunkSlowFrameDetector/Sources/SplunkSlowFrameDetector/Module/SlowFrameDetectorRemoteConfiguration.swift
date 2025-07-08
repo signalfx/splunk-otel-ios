88348bb0818e635e8ea7d8d19e65071623763514
Carlos McEvilly <cmcevill@cisco.com>
2025-07-08 00:30:54 -0700

    DEMRUM-1758: simplify remote config and move into its own file

--- SplunkSlowFrameDetector/Sources/SplunkSlowFrameDetector/Module/SlowFrameDetectorRemoteConfiguration.swift
+++ SplunkSlowFrameDetector/Sources/SplunkSlowFrameDetector/Module/SlowFrameDetectorRemoteConfiguration.swift
@@ -1,35 +1,33 @@
+//
+//
+/*
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import Foundation
+import SplunkCommon
+
+/// SlowFrameDetector module remote configuration.
 public struct SlowFrameDetectorRemoteConfiguration: RemoteModuleConfiguration {
 
-    // MARK: - Internal decoding
+    // MARK: - Module management
 
-    struct SlowFrameDetector: Decodable {
-        let enabled: Bool
-    }
-
-    struct MRUMRoot: Decodable {
-        let slowFrameDetector: SlowFrameDetector
-    }
-
-    struct Configuration: Decodable {
-        let mrum: MRUMRoot
-    }
-
-    struct Root: Decodable {
-        let configuration: Configuration
-    }
+    public var enabled: Bool = true
 
 
-    // MARK: - Protocol conformance
+    // MARK: - Initialization
 
-
-    // MARK: - Internal variables
-
-    public var enabled: Bool
-
-    public init?(from data: Data) {
-        guard let root = try? JSONDecoder().decode(Root.self, from: data) else {
-            return nil
-        }
-        enabled = root.configuration.mrum.slowFrameDetector.enabled
-    }
+    public init?(from data: Data) {}
 }
