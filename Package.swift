c9b970babaede56643be469b05e6298e7a303dd4
Jiri Charvat <jcharvat@splunk.com>
2025-07-14 13:22:57 +0200

    Update package manifest to support iOS only.

--- Package.swift
+++ Package.swift
@@ -9,10 +9,7 @@ import PackageDescription
 let package = Package(
     name: "SplunkAgent",
     platforms: [
-        .iOS(.v15),
-        .tvOS(.v15),
-        .visionOS(.v1),
-        .macCatalyst(.v13)
+        .iOS(.v15)
     ],
     products: [
         .library(
