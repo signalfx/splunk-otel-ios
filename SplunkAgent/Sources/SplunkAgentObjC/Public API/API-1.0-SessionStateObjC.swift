b4648ef8022354f0aaa38be74605cade506120ec
Václav Halík <vhalik@splunk.com>
2025-07-24 16:26:26 +0200

    DEMRUM-2797 - Implementation of classes for state and some of the configuration in the new Objective-C API target.

---
+++ SplunkAgent/Sources/SplunkAgentObjC/Public API/API-1.0-SessionStateObjC.swift
@@ -0,0 +1,52 @@
+//
+/*
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import Foundation
+import SplunkAgent
+
+/// A state object that reflects the current session's state.
+@objc(SPLKSessionState)
+public final class SessionStateObjC: NSObject {
+
+    // MARK: - Internal
+
+    private unowned let owner: SplunkRumObjC
+
+
+    // MARK: - Public API
+
+    /// Identification of recorded session.
+    ///
+    /// When the agent is initialized, there is always some session ID.
+    @objc(sessionID)
+    public var id: String {
+        owner.agent.session.state.id
+    }
+
+    /// Value of the currently used session sampling rate.
+    @objc
+    public var samplingRate: Double {
+        owner.agent.session.state.samplingRate
+    }
+
+
+    // MARK: - Initialization
+
+    init(for owner: SplunkRumObjC) {
+        self.owner = owner
+    }
+}
