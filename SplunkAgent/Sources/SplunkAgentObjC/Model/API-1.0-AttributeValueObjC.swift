ffe7ca89f4a4130332b5be478d62544ac6cd80ef
Václav Halík <vhalik@splunk.com>
2025-08-11 10:10:49 +0200

    DEMRUM-2905 - Add basic implementation for OTel `AttributeValue` in Objective-C API.

---
+++ SplunkAgent/Sources/SplunkAgentObjC/Model/API-1.0-AttributeValueObjC.swift
@@ -0,0 +1,269 @@
+//
+/*
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import Foundation
+import OpenTelemetryApi
+
+/// Object wrapper for basic attribute types used in OpenTelemetry.
+///
+/// The supported value types are `NSString`, `NSInteger`, `BOOL`, and `double`.
+@objc(SPLKAttributeValue) @objcMembers
+public final class AttributeValueObjC: NSObject {
+
+    // MARK: - Private
+
+    let value: Any
+
+
+    // MARK: - Public
+
+    /// Identifies the type of attribute value held.
+    public let type: AttributeValueTypeObjC
+
+
+    // MARK: - Initialization
+
+    /// Initializes a new instance of an attribute for a `BOOL` type value.
+    ///
+    /// - Parameter bool: The value for the new attribute.
+    public convenience init(bool: Bool) {
+        self.init(boolNumber: NSNumber(value: bool))
+    }
+
+    /// Initializes a new instance of an attribute for a `BOOL` type value, encapsulated as a `NSNumber`.
+    ///
+    /// - Parameter boolNumber: The value for the new attribute, encapsulated as a `NSNumber`.
+    public init(boolNumber: NSNumber) {
+        value = boolNumber
+        type = .bool
+    }
+
+
+    /// Initializes a new instance of an attribute for a `double` type value.
+    ///
+    /// - Parameter double: The value for the new attribute.
+    public convenience init(double: Double) {
+        self.init(doubleNumber: NSNumber(value: double))
+    }
+    
+    /// Initializes a new instance of an attribute for a `double` type value, encapsulated as a `NSNumber`.
+    ///
+    /// - Parameter doubleNumber: The value for the new attribute, encapsulated as a `NSNumber`.
+    public init(doubleNumber: NSNumber) {
+        value = doubleNumber
+        type = .double
+    }
+
+
+    /// Initializes a new instance of an attribute for a `NSInteger` type value.
+    ///
+    /// - Parameter integer: The value for the new attribute.
+    public convenience init(integer: Int) {
+        self.init(integerNumber: NSNumber(value: integer))
+    }
+
+    /// Initializes a new instance of an attribute for a `NSInteger` type value, encapsulated as a `NSNumber`.
+    ///
+    /// - Parameter integerNumber: The value for the new attribute, encapsulated as a `NSNumber`.
+    public init(integerNumber: NSNumber) {
+        value = integerNumber
+        type = .integer
+    }
+
+
+    /// Initializes a new instance of an attribute for an `NSString` type value.
+    ///
+    /// - Parameter string: A `NSString` with value for the new attribute.
+    public init(string: String) {
+        value = string
+        type = .string
+    }
+
+
+    // MARK: - Typed getters
+    
+    /// The value of the `BOOL` attribute, encapsulated as a `NSNumber`.
+    ///
+    /// If the attribute has not been initialized with a `BOOL`, it will return `nil`.
+    public var asBoolNumber: NSNumber? {
+        guard type == .bool else {
+            return nil
+        }
+        
+        return value as? NSNumber
+    }
+
+    /// The value of the `double` attribute, encapsulated as a `NSNumber`.
+    ///
+    /// If the attribute has not been initialized with a `double`, it will return `nil`.
+    public var asDoubleNumber: NSNumber? {
+        guard type == .double else {
+            return nil
+        }
+
+        return value as? NSNumber
+    }
+
+    /// The value of the `NSInteger` attribute, encapsulated as a `NSNumber`.
+    ///
+    /// If the attribute has not been initialized with a `NSInteger`, it will return `nil`.
+    public var asIntegerNumber: NSNumber? {
+        guard type == .integer else {
+            return nil
+        }
+
+        return value as? NSNumber
+    }
+
+    /// The value of the `NSString` attribute.
+    ///
+    /// If the attribute has not been initialized with a `NSString`, it will return `nil`.
+    public var asString: NSString? {
+        return value as? NSString
+    }
+    
+    
+    // MARK: - Factory methods
+    
+    /// Creates a new instance of an attribute for a `BOOL` type value.
+    ///
+    /// - Parameter bool: The value for the new attribute.
+    ///
+    /// - Returns: A new `BOOL` attribute, encapsulated as a `NSNumber`.
+    public static func attributeWithBool(_ bool: Bool) -> AttributeValueObjC {
+        return .init(bool: bool)
+    }
+
+    /// Creates a new instance of an attribute for a `double` type value.
+    ///
+    /// - Parameter double: The value for the new attribute.
+    ///
+    /// - Returns: A new `double` attribute, encapsulated as a `NSNumber`.
+    public static func attributeWithDouble(_ double: Double) -> AttributeValueObjC {
+        return .init(double: double)
+    }
+
+    /// Creates a new instance of an attribute for a `NSInteger` type value.
+    ///
+    /// - Parameter integer: The value for the new attribute.
+    ///
+    /// - Returns: A new `NSInteger` attribute, encapsulated as a `NSNumber`.
+    public static func attributeWithInteger(_ integer: Int) -> AttributeValueObjC {
+        return .init(integer: integer)
+    }
+    
+    /// Creates a new instance of an attribute for a `NSString` type value.
+    ///
+    /// - Parameter string: A `NSString` with value for the new attribute.
+    ///
+    /// - Returns: A new `NSString` attribute.
+    public static func attributeWithString(_ string: String) -> AttributeValueObjC {
+        return .init(string: string)
+    }
+}
+
+
+extension AttributeValueObjC {
+
+    // MARK: - Computed properties
+
+    var otelAttributeValue: AttributeValue? {
+        switch type {
+        case .bool:
+            if let boolValue = asBoolNumber?.boolValue {
+                return AttributeValue.bool(boolValue)
+            }
+
+        case .double:
+            if let doubleValue = asDoubleNumber?.doubleValue {
+                return AttributeValue.double(doubleValue)
+            }
+
+        case .integer:
+            if let intValue = asIntegerNumber?.intValue {
+                return AttributeValue.int(intValue)
+            }
+
+        case .string:
+            if let stringValue = asString {
+                return AttributeValue.string(String(stringValue))
+            }
+        }
+
+        return nil
+    }
+
+
+    // MARK: - Conversion init
+
+    convenience init?(with otelAttributeValue: AttributeValue) {
+        switch otelAttributeValue {
+        case .bool(let bool):
+            self.init(bool: bool)
+
+        case .double(let double):
+            self.init(double: double)
+
+        case .int(let int):
+            self.init(integer: int)
+
+        case .string(let string):
+            self.init(string: string)
+
+        default:
+            return nil
+        }
+    }
+}
+
+
+public extension AttributeValueObjC {
+
+    // MARK: - String convertible
+
+    /// A human-readable string representation of the `SPLKAttributeValue` instance.
+    override var description: String {
+        switch type {
+        case .bool:
+            if let boolValue = asBoolNumber?.boolValue {
+                return String(describing: boolValue)
+            }
+
+        case .double:
+            if let doubleValue = asDoubleNumber?.doubleValue {
+                return String(describing: doubleValue)
+            }
+
+        case .integer:
+            if let integerValue = asIntegerNumber?.intValue {
+                return String(describing: integerValue)
+            }
+
+        case .string:
+            if let stringValue = value as? String {
+                return stringValue.description
+            }
+        }
+
+        return "unknown"
+    }
+
+    /// A string representation of an `SPLKAttributeValue` instance intended for diagnostic output.
+    override var debugDescription: String {
+        return "SPLKAttributeValue<\(type)>: \(description)"
+    }
+}
---
+++ SplunkAgent/Tests/SplunkAgentObjCTests/PublicAPI/Model/API-1.0-AttributeValueObjCTests.m
@@ -0,0 +1,213 @@
+//
+/*
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#import <XCTest/XCTest.h>
+
+@import SplunkAgentObjC;
+
+@interface API10AttributeValueObjCTests : XCTestCase
+
+// MARK: - Constants
+
+@property (nonatomic, readonly) BOOL sampleBool;
+@property (nonatomic, readonly) double sampleDouble;
+@property (nonatomic, readonly) NSInteger sampleInteger;
+@property (nonatomic, readonly) NSString *sampleString;
+
+@end
+
+
+@implementation API10AttributeValueObjCTests
+
+// MARK: - Constants
+
+- (BOOL)sampleBool {
+    return NO;
+}
+
+- (double)sampleDouble {
+    return 1.0;
+}
+
+- (NSInteger)sampleInteger {
+    return -1;
+}
+
+- (NSString *)sampleString {
+    return @"Test";
+}
+
+
+// MARK: - API Tests
+
+- (void)testInitialization {
+    // Initialization with native types
+    SPLKAttributeValue *boolAttribute = [[SPLKAttributeValue alloc] initWithBool:self.sampleBool];
+    SPLKAttributeValue *doubleAttribute = [[SPLKAttributeValue alloc] initWithDouble:self.sampleDouble];
+    SPLKAttributeValue *integerAttribute = [[SPLKAttributeValue alloc] initWithInteger:self.sampleInteger];
+    SPLKAttributeValue *stringAttribute = [[SPLKAttributeValue alloc] initWithString:self.sampleString];
+
+    // Initialization with encapsulation into NSNumber
+    NSNumber *boolNumber = [NSNumber numberWithBool:self.sampleBool];
+    SPLKAttributeValue *boolNumberAttribute = [[SPLKAttributeValue alloc] initWithBoolNumber:boolNumber];
+
+    NSNumber *doubleNumber = [NSNumber numberWithDouble:self.sampleDouble];
+    SPLKAttributeValue *doubleNumberAttribute = [[SPLKAttributeValue alloc] initWithDoubleNumber:doubleNumber];
+
+    NSNumber *integerNumber = [NSNumber numberWithInteger:self.sampleInteger];
+    SPLKAttributeValue *integerNumberAttribute = [[SPLKAttributeValue alloc] initWithIntegerNumber:integerNumber];
+
+
+    XCTAssertNotNil(boolAttribute);
+    XCTAssertNotNil(doubleAttribute);
+    XCTAssertNotNil(integerAttribute);
+    XCTAssertNotNil(stringAttribute);
+
+    XCTAssertNotNil(boolNumberAttribute);
+    XCTAssertNotNil(doubleNumberAttribute);
+    XCTAssertNotNil(integerNumberAttribute);
+}
+
+- (void)testFactoryMethods {
+    SPLKAttributeValue *boolAttribute = [SPLKAttributeValue attributeWithBool:self.sampleBool];
+    SPLKAttributeValue *doubleAttribute = [SPLKAttributeValue attributeWithDouble:self.sampleDouble];
+    SPLKAttributeValue *integerAttribute = [SPLKAttributeValue attributeWithInteger:self.sampleInteger];
+    SPLKAttributeValue *stringAttribute = [SPLKAttributeValue attributeWithString:self.sampleString];
+
+    BOOL storedBool = boolAttribute.asBoolNumber.boolValue;
+    double storedDouble = doubleAttribute.asDoubleNumber.doubleValue;
+    NSInteger storedInteger = integerAttribute.asIntegerNumber.integerValue;
+    NSString *storedString = stringAttribute.asString;
+
+
+    XCTAssertEqual(storedBool, self.sampleBool);
+    XCTAssertEqual(boolAttribute.type, SPLKAttributeValueTypeBool);
+
+    XCTAssertEqual(storedDouble, self.sampleDouble);
+    XCTAssertEqual(doubleAttribute.type, SPLKAttributeValueTypeDouble);
+
+    XCTAssertEqual(storedInteger, self.sampleInteger);
+    XCTAssertEqual(integerAttribute.type, SPLKAttributeValueTypeInteger);
+
+    XCTAssertTrue([storedString isEqualToString:self.sampleString]);
+    XCTAssertEqual(stringAttribute.type, SPLKAttributeValueTypeString);
+}
+
+- (void)testBoolTypedGetters {
+    SPLKAttributeValue *boolAttribute = [[SPLKAttributeValue alloc] initWithBool:self.sampleBool];
+
+    // Access using getter for stored type
+    NSNumber *storedBoolNumber = boolAttribute.asBoolNumber;
+    BOOL storedBool = storedBoolNumber.boolValue;
+    SPLKAttributeValueType storedType = boolAttribute.type;
+
+    // Access using non-compatible getters should return `nil`
+    NSNumber *storedDoubleNumber = boolAttribute.asDoubleNumber;
+    NSNumber *storedIntegerNumber = boolAttribute.asIntegerNumber;
+    NSString *storedString = boolAttribute.asString;
+
+
+    XCTAssertNotNil(storedBoolNumber);
+    XCTAssertEqual(storedBool, self.sampleBool);
+    XCTAssertEqual(storedType, SPLKAttributeValueTypeBool);
+
+    XCTAssertNil(storedDoubleNumber);
+    XCTAssertNil(storedIntegerNumber);
+    XCTAssertNil(storedString);
+}
+
+- (void)testDoubleTypedGetters {
+    SPLKAttributeValue *doubleAttribute = [[SPLKAttributeValue alloc] initWithDouble:self.sampleDouble];
+
+    // Access using getter for stored type
+    NSNumber *storedDoubleNumber = doubleAttribute.asDoubleNumber;
+    double storedDouble = storedDoubleNumber.doubleValue;
+    SPLKAttributeValueType storedType = doubleAttribute.type;
+
+    // Access using non-compatible getters should return `nil`
+    NSNumber *storedBoolNumber = doubleAttribute.asBoolNumber;
+    NSNumber *storedIntegerNumber = doubleAttribute.asIntegerNumber;
+    NSString *storedString = doubleAttribute.asString;
+
+
+    XCTAssertNotNil(storedDoubleNumber);
+    XCTAssertEqual(storedDouble, self.sampleDouble);
+    XCTAssertEqual(storedType, SPLKAttributeValueTypeDouble);
+
+    XCTAssertNil(storedBoolNumber);
+    XCTAssertNil(storedIntegerNumber);
+    XCTAssertNil(storedString);
+}
+
+- (void)testIntegerTypedGetters {
+    SPLKAttributeValue *integerAttribute = [[SPLKAttributeValue alloc] initWithInteger:self.sampleInteger];
+
+    // Access using getter for stored type
+    NSNumber *storedIntegerNumber = integerAttribute.asIntegerNumber;
+    NSInteger storedInteger = storedIntegerNumber.integerValue;
+    SPLKAttributeValueType storedType = integerAttribute.type;
+
+    // Access using non-compatible getters should return `nil`
+    NSNumber *storedBoolNumber = integerAttribute.asBoolNumber;
+    NSNumber *storedDoubleNumber = integerAttribute.asDoubleNumber;
+    NSString *storedString = integerAttribute.asString;
+
+
+    XCTAssertNotNil(storedIntegerNumber);
+    XCTAssertEqual(storedInteger, self.sampleInteger);
+    XCTAssertEqual(storedType, SPLKAttributeValueTypeInteger);
+
+    XCTAssertNil(storedBoolNumber);
+    XCTAssertNil(storedDoubleNumber);
+    XCTAssertNil(storedString);
+}
+
+- (void)testStringTypedGetters {
+    SPLKAttributeValue *stringAttribute = [[SPLKAttributeValue alloc] initWithString:self.sampleString];
+
+    // Access using getter for stored type
+    NSString *storedString = stringAttribute.asString;
+    SPLKAttributeValueType storedType = stringAttribute.type;
+
+    // Access using non-compatible getters should return `nil`
+    NSNumber *storedBoolNumber = stringAttribute.asBoolNumber;
+    NSNumber *storedDoubleNumber = stringAttribute.asDoubleNumber;
+    NSNumber *storedIntegerNumber = stringAttribute.asIntegerNumber;
+
+
+    XCTAssertNotNil(storedString);
+    XCTAssertTrue([storedString isEqualToString:self.sampleString]);
+    XCTAssertEqual(storedType, SPLKAttributeValueTypeString);
+
+    XCTAssertNil(storedBoolNumber);
+    XCTAssertNil(storedDoubleNumber);
+    XCTAssertNil(storedIntegerNumber);
+}
+
+- (void)testAttributeValueTypes {
+    SPLKAttributeValueType boolType = SPLKAttributeValueTypeBool;
+    SPLKAttributeValueType doubleType = SPLKAttributeValueTypeDouble;
+    SPLKAttributeValueType integerType = SPLKAttributeValueTypeInteger;
+    SPLKAttributeValueType stringType = SPLKAttributeValueTypeString;
+
+    XCTAssertEqual(boolType, 0);
+    XCTAssertEqual(doubleType, 1);
+    XCTAssertEqual(integerType, 2);
+    XCTAssertEqual(stringType, 3);
+}
+
+@end
