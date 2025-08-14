93cdff94555bf049e641668891331cd27e8b434e
Václav Halík <vhalik@cisco.com>
2025-08-14 11:44:38 +0200

    DEMRUM-2905: Objective-C API for Global Attributes (#390)
    * DENRUM-2856 - Fix Access Control warnings from new Linter versions.
    
    * DEMRUM-2797 - Implementation of model classes for the new Objective-C API target.
    
    * DEMRUM-2797 - Implementation of classes for state and some of the configuration in the new Objective-C API target.
    
    * DEMRUM-2797 - Implementation of classes for session in the new Objective-C API target.
    
    * DEMRUM-2797 - Implementation of the remaining agent core classes in the new Objective-C API target.
    
    * DEMRUM-2797 - Add Unit Tests covering Objective-C API for Agent core functionalities.
    
    * DEMRUM-2797 - Add agent builder for instances used in Objective-C API tests.
    
    * DEMRUM-2797 - Fix for warnings from linters.
    
    * DEMRUM-2905 - Add basic implementation for OTel `AttributeValue` in Objective-C API.
    
    * DEMRUM-2905 - API update for global attributes in the Objective-C interface.
    
    * DEMRUM-2905 - Extended accessibility of error messages from agent configuration.
    
    * DEMRUM-2905 - Modification of the parameter naming scheme in the  `AttributeValue` for Objective-C API.

---
+++ SplunkAgent/Sources/SplunkAgentObjC/Model Conversions/MutableAttributes+Conversions.swift
@@ -0,0 +1,51 @@
+//
+/*
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+import Foundation
+import OpenTelemetryApi
+import SplunkAgent
+
+extension MutableAttributes {
+
+    // MARK: - Computed properties
+
+    var attributesDictionary: [String: AttributeValueObjC] {
+        let attributes: [String: AttributeValue] = getAll()
+        var attributesDictionary = [String: AttributeValueObjC]()
+
+        // Map OpenTelemetry attributes to Objective-C
+        for (key, value) in attributes {
+            attributesDictionary[key] = AttributeValueObjC(with: value)
+        }
+
+        return attributesDictionary
+    }
+
+
+    // MARK: - Conversion init
+
+    convenience init(with attributesDictionary: [String: AttributeValueObjC]) {
+        // Map Objective-C dictionary to OpenTelemetry attributes
+        let attributes: [String: AttributeValue] = attributesDictionary.reduce(into: [:]) { result, element in
+            if let attributeValue = element.value.otelAttributeValue {
+                result[element.key] = attributeValue
+            }
+        }
+
+        self.init(dictionary: attributes)
+    }
+}
