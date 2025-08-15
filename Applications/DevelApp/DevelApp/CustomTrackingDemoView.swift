7d082767cd86c23b5c6fd12eebe504e9bd445fcd
SMickelsn <122052675+SMickelsn@users.noreply.github.com>
2025-08-15 14:39:14 -0700

    DEMRUM-2683: Update Custom Tracking Demo

--- Applications/DevelApp/DevelApp/CustomTrackingDemoView.swift
+++ Applications/DevelApp/DevelApp/CustomTrackingDemoView.swift
@@ -112,6 +112,7 @@ struct CustomTrackingDemoView: View {
 
     func trackWorkflow() {
         let customSpan = SplunkRum.shared.customTracking.trackWorkflow("Custom Workflow")
+        customSpan.setAttribute(key: "test", value: nil)
         customSpan.setAttribute(key: "test", value: "qwerty")
 
         DispatchQueue.main.asyncAfter(deadline: .now() + 15) {
