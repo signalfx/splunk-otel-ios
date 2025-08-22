99881979d39b1af510212c6a1e7085eff90d7bba
Filip Ruzicka <ruzicka@cisco.com>
2025-08-22 16:00:57 +0200

    Remove build scripts

--- Tools/build_frameworks/000-Bootstrap.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Root path to the project. Assuming the main script,
-# which path is in $SCRIPT_DIR read in the main script itself,
-# is in Tools subdir of the project folder.
-pushd "${SCRIPT_DIR}/.."  > /dev/null
-PROJECT_DIR="$(pwd)"
-popd > /dev/null
