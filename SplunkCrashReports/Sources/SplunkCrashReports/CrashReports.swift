e4e2ec54095c658d7711587261ba19bc27dabeee
SMickelsn <122052675+SMickelsn@users.noreply.github.com>
2025-08-22 17:17:06 -0700

    NO-TICKET: Update per review

--- SplunkCrashReports/Sources/SplunkCrashReports/CrashReports.swift
+++ SplunkCrashReports/Sources/SplunkCrashReports/CrashReports.swift
@@ -120,18 +120,17 @@ public class CrashReports {
             let reportPayload = formatCrashReport(report: report)
 
             // Fetch the crash timestamp
-            var timestamp = Date()  // Default to now, if no sytemInfo, should not ever happen
+            var timestamp = Date() // Default to now, if no sytemInfo, should not ever happen
             if let systemInfo = report.systemInfo {
-                timestamp = report.systemInfo.timestamp
-            }
-            else {
+                timestamp = systemInfo.timestamp
+            } else {
                 logger.log(level: .error) {
                     "CrashReporter failed to report systemInfo timestamp"
                 }
             }
 
             // Send the report to the backend
-            send(crashReport: reportPayload, sharedState: sharedState, timestamp: report.systemInfo.timestamp)
+            send(crashReport: reportPayload, sharedState: sharedState, timestamp: timestamp)
         } catch {
             logger.log(level: .error) {
                 "CrashReporter failed to load/parse with error: \(error)"
@@ -258,9 +257,8 @@ public class CrashReports {
         if let sharedState {
             let timebasedAppState = sharedState.applicationState(for: report.systemInfo.timestamp) ?? "unknown"
 
-            // TODO: This mapping code should be removed in favor of returning the line above
-            // once the backend is able to support it.
-
+            // This mapping code below may be able to be removed in the future should
+            // the backend is able to support all options.
             appState = switch timebasedAppState {
             case "active": "foreground"
             case "inactive", "terminate": "background"
