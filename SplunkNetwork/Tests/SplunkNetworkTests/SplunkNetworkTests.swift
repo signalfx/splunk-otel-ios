a446df13eb735440bd49e5057f0be366d7a5d439
SMickelsn <122052675+SMickelsn@users.noreply.github.com>
2025-11-08 18:10:28 -0800

    DEMRUM-3819: Remove Old Network Tests

--- SplunkNetwork/Tests/SplunkNetworkTests/SplunkNetworkTests.swift
+++ SplunkNetwork/Tests/SplunkNetworkTests/SplunkNetworkTests.swift
@@ -50,19 +50,19 @@ final class SplunkNetworkTests: XCTestCase {
         var totalAttributeCount: Int = 0
         var parentSpanId: SpanId?
         var instrumentationScopeInfo = InstrumentationScopeInfo()
-        
-        
+
+
         // MARK: - SpanBase properties
-        
+
         var kind: SpanKind { .internal }
         var context: SpanContext
         var isRecording: Bool { true }
         var status: Status = .unset
         var name: String = "MockSpan"
-        
-        
+
+
         // MARK: - Intialization
-        
+
         init() {
             let traceId = TraceId.random()
             let spanId = SpanId.random()
@@ -75,10 +75,10 @@ final class SplunkNetworkTests: XCTestCase {
                 traceState: traceState
             )
         }
-        
-        
+
+
         // MARK: - Attributes
-        
+
         func setAttribute(key: String, value: Any) {
             if let stringValue = value as? String {
                 attributes[key] = .string(stringValue)
@@ -93,53 +93,53 @@ final class SplunkNetworkTests: XCTestCase {
                 attributes[key] = .bool(boolValue)
             }
         }
-        
-        
+
+
         // MARK: - SpanBase methods
-        
+
         func setAttribute(key: String, value: AttributeValue?) {
             if let value {
                 attributes[key] = value
             }
         }
-        
+
         func setAttributes(_ attributes: [String: OpenTelemetryApi.AttributeValue]) {
             self.attributes = attributes
         }
-        
+
         func addEvent(name _: String) {}
         func addEvent(name _: String, timestamp _: Date) {}
         func addEvent(name _: String, attributes _: [String: AttributeValue]) {}
         func addEvent(name _: String, attributes _: [String: AttributeValue], timestamp _: Date) {}
-        
-        
+
+
         // MARK: - Span methods
-        
+
         func end() {}
         func end(time _: Date) {}
-        
-        
+
+
         // MARK: - SpanExceptionRecorder methods
-        
+
         func recordException(_: SpanException) {}
         func recordException(_: SpanException, timestamp _: Date) {}
         func recordException(_: SpanException, attributes _: [String: AttributeValue]) {}
         func recordException(_: SpanException, attributes _: [String: AttributeValue], timestamp _: Date) {}
-        
+
         // MARK: - CustomStringConvertible
-        
+
         var description: String { "MockSpan" }
     }
-    
+
     override func setUp() {
         super.setUp()
-        
+
         sut = NetworkInstrumentation()
     }
-    
+
     override func tearDown() {
         sut = nil
-        
+
         super.tearDown()
     }
 }
