ab05f3ce56b5e58a4ae2ea0e5c25aef8fdfe2191
Carlos McEvilly <cmcevill@cisco.com>
2026-02-27 18:15:26 -0800

    DEMRUM-4775: Add navigation detection source seams

---
+++ SplunkNavigation/Sources/SplunkNavigation/NavigationEventSources.swift
@@ -0,0 +1,44 @@
+//
+/*
+Copyright 2025 Splunk Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+internal import CiscoSwizzling
+import Foundation
+
+/// Provides navigation action events produced by swizzling.
+protocol NavigationEventStreamProviding {
+    func navigationStream() async throws -> AsyncStream<any NavigationActionEvent>
+}
+
+/// Default stream provider backed by CiscoSwizzling.
+struct DefaultNavigationEventStreamProvider: NavigationEventStreamProviding {
+    func navigationStream() async throws -> AsyncStream<any NavigationActionEvent> {
+        try await DefaultSwizzling.navigation
+    }
+}
+
+/// Abstracts notification source used by legacy detection.
+protocol NotificationEventsProviding {
+    func notifications(for name: Notification.Name) -> AsyncStream<Notification>
+}
+
+extension NotificationCenter: NotificationEventsProviding {}
+
+/// Navigation detection mode used by the module.
+enum NavigationDetectionMode {
+    case legacy
+    case modern
+}
