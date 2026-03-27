33add5b225cdf69b66f04ca241c33cef2bf8430e
Carlos McEvilly <cmcevill@cisco.com>
2026-03-26 19:46:44 -0700

    DEMRUM-4775: make AutomatedNavigationEvent swiftformat-compliant

--- SplunkNavigation/Sources/SplunkNavigation/Model/Legacy/AutomatedNavigationEvent.swift
+++ SplunkNavigation/Sources/SplunkNavigation/Model/Legacy/AutomatedNavigationEvent.swift
@@ -29,7 +29,10 @@ struct AutomatedNavigationEvent: NavigationActionEvent {
     var type: NavigationActionEventType
     var controllerTypeName: String
     var controllerIdentifier: ObjectIdentifier
-    // Required for NavigationActionEvent conformance.
-    var navigationControllerIdentifier: ObjectIdentifier? { nil }
+    var navigationControllerIdentifier: ObjectIdentifier? {
+        // Required for NavigationActionEvent conformance.
+        nil
+    }
+
     var viewFrame: CGRect?
 }
