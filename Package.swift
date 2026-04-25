118aa404c3f05c908fcf721a881d1792718f6559
Carlos McEvilly <cmcevill@cisco.com>
2026-04-24 17:18:47 -0700

    DEMRUM-5290: Remove ObjC bridge tests from SplunkAgentTests
    * Remove NavigationConfigurationObjCConversionTests,
      NavigationEventProcessorAdapterTests, and
      MockNavigationEventProcessorsObjC from SplunkAgentTests
    * Revert SplunkAgentObjC dependency from SplunkAgentTests
    * Tests will be restored in SplunkAgentBridgingTests target

--- Package.swift
+++ Package.swift
@@ -96,7 +96,7 @@ func generateMainTargets() -> [Target] {
         ),
         .testTarget(
             name: "SplunkAgentTests",
-            dependencies: ["SplunkAgent", "SplunkAgentObjC", "SplunkCommon"],
+            dependencies: ["SplunkAgent", "SplunkCommon"],
             path: "SplunkAgent/Tests/SplunkAgentTests",
             resources: [
                 .copy("Testing Support/Assets/v.mp4"),
