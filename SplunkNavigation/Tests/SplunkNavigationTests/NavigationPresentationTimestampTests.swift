08dd7b1c7a09d1d6ae8de702fc262475960444e5
Carlos McEvilly <cmcevill@cisco.com>
2026-06-02 09:48:27 -0700

    DEMRUM-5585: remove needless import (PR review feedback)

--- SplunkNavigation/Tests/SplunkNavigationTests/NavigationPresentationTimestampTests.swift
+++ SplunkNavigation/Tests/SplunkNavigationTests/NavigationPresentationTimestampTests.swift
@@ -15,7 +15,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-internal import CiscoSwizzling
 import Foundation
 @_spi(SplunkTesting) import SplunkCommon
 import UIKit
