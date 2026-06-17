9dcdb7c5b8578ab03493587586c8d2553529f8f0
Carlos McEvilly <cmcevill@cisco.com>
2026-06-16 21:04:06 -0700

    DEMRUM-5585: Remove -retry-tests-on-failure from CI test script

--- .github/actions/build-and-tests/scripts/test.sh
+++ .github/actions/build-and-tests/scripts/test.sh
@@ -14,4 +14,4 @@ LOG="$OUT_DIR/xcodebuild-ios.log"
 JUNIT="$OUT_DIR/junit-ios.xml"
 XCRESULT="${XCRESULT_PATH:-TestResults_iOS.xcresult}"
 
-xcodebuild -skipPackagePluginValidation -testPlan "SplunkAgent" -workspace "$WS" -scheme "$SCHEME" -destination "$DESTINATION" -resultBundlePath "$XCRESULT" clean test -retry-tests-on-failure 2>&1 | tee "$LOG" | xcbeautify --report junit --junit-report-filename "$(basename "$JUNIT")" --report-path "$OUT_DIR"
+xcodebuild -skipPackagePluginValidation -testPlan "SplunkAgent" -workspace "$WS" -scheme "$SCHEME" -destination "$DESTINATION" -resultBundlePath "$XCRESULT" clean test 2>&1 | tee "$LOG" | xcbeautify --report junit --junit-report-filename "$(basename "$JUNIT")" --report-path "$OUT_DIR"
